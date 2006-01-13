6b53beb3b9c2d220cff62447b30806ddfaaf3be5
hbitteur <herve.bitteur@audiveris.org>
2006-01-13 23:01:16 +0000

    Redefined as an Interface

--- src/main/omr/math/Line.java
+++ src/main/omr/math/Line.java
@@ -13,189 +13,181 @@ package omr.math;
 import omr.util.Logger;
 
 /**
- * Class <code>Line</code> handles the equation of a line, in the form y =
- * ax + b.
- *
- * <p> Note that this is not suitable for near vertical lines, which would
- * be better described by the VerticalLine class. </p>
+ * Interface <code>Line</code> handles the equation of a line, whatever its
+ * orientation, using the form : a*x + b*y + c = 0. The equation is
+ * normalized, in other words, we always have a**2 + b**2 = 1.
  *
  * @author Herv&eacute Bitteur
  * @version $Id$
  */
-public class Line
-        implements java.io.Serializable
+public interface Line
+    extends java.io.Serializable
 {
     //~ Static variables/initializers -------------------------------------
 
-    private static final Logger logger = Logger.getLogger(Line.class);
+    /** The same logger, usable by all subclasses */
+    static final Logger logger = Logger.getLogger(Line.class);
 
-    //~ Instance variables ------------------------------------------------
-
-    // Coeffs of y = a.x +b
-
-    /** slope */
-    protected double a;
-
-    /** offset */
-    protected double b;
-
-    /** normalizing factor : sqrt(1 + (a**2) */
-    protected double norm;
-
-    // For incremental fitting
-
-    /** Number of points */
-    protected int n;
-
-    /** Sigma (x) */
-    protected double sx;
-
-    /** Sigma (y) */
-    protected double sy;
-
-    /** Sigma (x**2) */
-    protected double sx2;
-
-    /** Sigma (y**2) */
-    protected double sy2;
-
-    /** Sigma (x*y) */
-    protected double sxy;
-
-    /** Flag to indicate that data needs to be recomputed */
-    protected boolean dirty;
-
-    //~ Constructors ------------------------------------------------------
-
-    //------//
-    // Line //
-    //------//
     /**
-     * Creates a line, with no data.
-     */
-    public Line ()
+     * Specific exception raised when trying to use a line with undefined
+     * parameters
+     * */
+    static class UndefinedLineException
+        extends RuntimeException
     {
-        reset();
-    }
-
-    //------//
-    // Line //
-    //------//
-    /**
-     * Creates a line, for which we already know the coefficients
-     *
-     * @param a slope
-     * @param b offset
-     */
-    public Line (double a,
-                 double b)
-    {
-        reset();
-        this.a = a;
-        this.b = b;
-        normalize();
-        dirty = false;
-    }
-
-    //------//
-    // Line //
-    //------//
-    /**
-     * Creates a line (and its coefficients), as the least square fitted
-     * line on the provided point population
-     *
-     * @param xVals abscissas of the points
-     * @param yVals ordinates of the points
-     */
-    public Line (double[] xVals,
-                 double[] yVals)
-    {
-        if (logger.isDebugEnabled()) {
-            logger.logAssert
-                (xVals.length == yVals.length,
-                 "leastSquareFit. xVals & yVals have different lengths");
-            logger.logAssert
-                (xVals.length >= 2,
-                 "leastSquareFit. Less than 2 points");
+        UndefinedLineException (String message)
+        {
+            super(message);
         }
-
-        reset();
-
-        for (int i = xVals.length - 1; i >= 0; i--) {
-            include(xVals[i], yVals[i]);
-        }
-
-        compute();
     }
 
     //~ Methods -----------------------------------------------------------
 
-    //-----------------//
-    // getMeanDistance //
-    //-----------------//
+    //------//
+    // getA //
+    //------//
     /**
-     * Return the mean distance of the defining population of point to the
-     * line. This can be used to measure how well the line fits the points
+     * Return a, the coefficient of x in a*x + b*y +c
      *
-     * @return this distance
+     * @return x coefficient
      */
-    public double getMeanDistance ()
-    {
-        if (dirty) {
-            compute();
-        }
+    double getA ();
 
-        return Math.sqrt(sy2 - (2 * a * sxy) - (2 * b * sy) + (a * a * sx2)
-                         + (2 * a * b * sx) + (n * b * b)) / n;
-    }
-
-    //-----------//
-    // getOffset //
-    //-----------//
+    //------//
+    // getB //
+    //------//
     /**
-     * Return the initial ordinate (the offset) of the line, that is the y
-     * value when x equals 0.
+     * Return b, the coefficient of y in a*x + b*y +c
      *
-     * @return the offset
+     * @return y coefficient
      */
-    public double getOffset ()
-    {
-        if (dirty) {
-            compute();
-        }
+    double getB ();
 
-        return b;
-    }
-
-    //------------//
-    // getPointNb //
-    //------------//
+    //------//
+    // getC //
+    //------//
     /**
-     * Return the cardinality of the population of defining points
+     * Return c, the coefficient of 1 in a*x + b*y +c
      *
-     * @return the number of defining points so far
+     * @return 1 coefficient
      */
-    public int getPointNb ()
-    {
-        return n;
-    }
+    double getC ();
 
     //----------//
     // getSlope //
     //----------//
     /**
-     * Return the gain of the line, that is the y/x coefficient
+     * Return -a/b, from a*x + b*y +c
      *
-     * @return the slope
+     * @return the y/x coefficient
      */
-    public double getSlope ()
-    {
-        if (dirty) {
-            compute();
-        }
+    double getSlope ();
 
-        return a;
-    }
+    //------------------//
+    // getInvertedSlope //
+    //------------------//
+    /**
+     * Return -b/a, from a*x + b*y +c
+     *
+     * @return the x/y coefficient
+     */
+    double getInvertedSlope ();
+
+    //-------------------//
+    // getNumberOfPoints //
+    //-------------------//
+    /**
+     * Return the cardinality of the population of defining points.
+     *
+     * @return the number of defining points so far
+     */
+    int getNumberOfPoints ();
+
+    //--------------//
+    // includePoint //
+    //--------------//
+    /**
+     * Add the coordinates of a point in the defining population of points.
+     *
+     * @param x abscissa of the new point
+     * @param y ordinate of the new point
+     */
+    void includePoint (double x,
+                       double y);
+
+    //-------------//
+    // includeLine //
+    //-------------//
+    /**
+     * Add the whole population of another line, which results in merging
+     * this other line with the line at hand.
+     *
+     * @param other the other line
+     *
+     * @return this augmented line, which permits to chain the additions.
+     */
+    Line includeLine (Line other);
+
+    //-------//
+    // reset //
+    //-------//
+    /**
+     * Remove the whole population of points. The line is not immediately
+     * usable, it needs now to include defining points.
+     */
+    void reset ();
+
+    //-----//
+    // xAt //
+    //-----//
+    /**
+     * Retrieve the abscissa where the line crosses the given ordinate y.
+     * Beware of horizontal lines !!!
+     *
+     * @param y the imposed ordinate
+     *
+     * @return the corresponding x value
+     */
+    double xAt (double y);
+
+    //-----//
+    // yAt //
+    //-----//
+    /**
+     * Retrieve the ordinate where the line crosses the given abscissa x,
+     * rounded to the nearest integer value.  Beware of vertical lines !!!
+     *
+     * @param x the imposed abscissa
+     *
+     * @return the corresponding y value
+     */
+    double yAt (double x);
+
+    //-----//
+    // xAt //
+    //-----//
+    /**
+     * Retrieve the abscissa where the line crosses the given ordinate y.
+     * Beware of horizontal lines !!!
+     *
+     * @param y the imposed ordinate
+     *
+     * @return the corresponding x value
+     */
+    int xAt (int y);
+
+    //-----//
+    // yAt //
+    //-----//
+    /**
+     * Retrieve the ordinate where the line crosses the given abscissa x,
+     * rounded to the nearest integer value.  Beware of vertical lines !!!
+     *
+     * @param x the imposed abscissa
+     *
+     * @return the corresponding y value
+     */
+    int yAt (int x);
 
     //------------//
     // distanceOf //
@@ -209,215 +201,18 @@ public class Line
      *
      * @return the algebraic orthogonal distance
      */
-    public double distanceOf (double x,
-                              double y)
-    {
-        if (dirty) {
-            compute();
-        }
+    double distanceOf (double x,
+                       double y);
 
-        return ((a * x) - y + b) / norm;
-    }
-
-    //---------//
-    // include //
-    //---------//
+    //-----------------//
+    // getMeanDistance //
+    //-----------------//
     /**
-     * Add the coordinates of a point in the defining population of points
+     * Return the mean quadratic distance of the defining population of
+     * points to the resulting line. This can be used to measure how well
+     * the line fits the points.
      *
-     * @param x abscissa of the new point
-     * @param y ordinate of the new point
+     * @return the absolute value of the mean distance
      */
-    public void include (double x,
-                         double y)
-    {
-        if (logger.isDebugEnabled()) {
-            logger.debug("include x=" + x + " y=" + y);
-        }
-
-        n += 1;
-        sx += x;
-        sy += y;
-        sx2 += (x * x);
-        sy2 += (y * y); // For distance
-        sxy += (x * y);
-        dirty = true;
-    }
-
-    //---------//
-    // include //
-    //---------//
-    /**
-     * Add the whole population of another line, which results in merging
-     * this other line with the line at hand.
-     *
-     * @param other the other line
-     *
-     * @return this auglented line, which permits to chain the additions
-     */
-    public Line include (Line other)
-    {
-        n += other.n;
-        sx += other.sx;
-        sy += other.sy;
-        sx2 += other.sx2;
-        sy2 += other.sy2;
-        sxy += other.sxy;
-        dirty = true;
-
-        return this;
-    }
-
-    //-------//
-    // reset //
-    //-------//
-    /**
-     * Remove the whole population of points
-     */
-    public void reset ()
-    {
-        a = b = norm = 0d;
-        n = 0;
-        sx = sy = sx2 = sy2 = sxy = 0d;
-        dirty = true;
-    }
-
-    //----------//
-    // toString //
-    //----------//
-    /**
-     * Return a readable equation for the line
-     *
-     * @return the string
-     */
-    public String toString ()
-    {
-        if (dirty) {
-            compute();
-        }
-
-        StringBuffer sb = new StringBuffer();
-        sb
-            .append("{Line")
-            .append(" y = ")
-            .append((float) a)
-            .append("*x ");
-
-        if (b >= 0) {
-            sb.append("+");
-        }
-
-        sb
-            .append((float) b)
-            .append("}");
-
-        return sb.toString();
-    }
-
-    //-----//
-    // xAt //
-    //-----//
-    /**
-     * Retrieve the abscissa where the line crosses the given ordinate y.
-     * Beware of horizontal lines !!!
-     *
-     * @param y the imposed ordinate
-     *
-     * @return the corresponding x value
-     */
-    public double xAt (double y)
-    {
-        if (dirty) {
-            compute();
-        }
-
-        return (y - b) / a;
-    }
-
-    //-----//
-    // xAt //
-    //-----//
-    /**
-     * Retrieve the abscissa where the line crosses the given ordinate y.
-     * Beware of horizontal lines !!!
-     *
-     * @param y the imposed ordinate
-     *
-     * @return the corresponding x value
-     */
-    public int xAt (int y)
-    {
-        return (int) Math.rint(xAt((double) y));
-    }
-
-    //-----//
-    // yAt //
-    //-----//
-    /**
-     * Retrieve the ordinate where the line crosses the given abscissa x.
-     * Beware of vertical lines !!!
-     *
-     * @param x the imposed abscissa
-     *
-     * @return the corresponding y value
-     */
-    public double yAt (double x)
-    {
-        if (dirty) {
-            compute();
-        }
-
-        return (a * x) + b;
-    }
-
-    //-----//
-    // yAt //
-    //-----//
-    /**
-     * Retrieve the ordinate where the line crosses the given abscissa x.
-     * Beware of vertical lines !!!
-     *
-     * @param x the imposed abscissa
-     *
-     * @return the corresponding y value
-     */
-    public int yAt (int x)
-    {
-        return (int) Math.rint(yAt((double) x));
-    }
-
-    //---------//
-    // compute //
-    //---------//
-    /**
-     * Compute the line equation, based on the cumulated number of points
-     */
-    protected void compute ()
-    {
-        if (logger.isDebugEnabled()) {
-            logger.logAssert(n >= 2, "compute. Less than 2 points");
-        }
-
-        double den = (n * sx2) - (sx * sx);
-
-        if (logger.isDebugEnabled()) {
-            logger.debug("den=" + den);
-        }
-
-        a = ((n * sxy) - (sx * sy)) / den;
-        b = ((sy * sx2) - (sx * sxy)) / den;
-        normalize();
-        dirty = false;
-    }
-
-    //-----------//
-    // normalize //
-    //-----------//
-    /**
-     * Compute the normalizing factor
-     */
-    protected void normalize ()
-    {
-        norm = Math.sqrt(1 + (a * a));
-    }
+    double getMeanDistance ();
 }
