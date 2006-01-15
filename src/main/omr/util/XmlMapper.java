117867cd669fa5044025f6168a9aefe6e4d93e5c
hbitteur <herve.bitteur@audiveris.org>
2006-01-15 17:42:18 +0000

    Use JiBX for the XML persistence utility, instead of Castor

--- src/main/omr/util/XmlMapper.java
+++ src/main/omr/util/XmlMapper.java
@@ -10,38 +10,37 @@
 
 package omr.util;
 
-import org.exolab.castor.mapping.Mapping;
-import org.exolab.castor.mapping.MappingException;
-import org.exolab.castor.xml.Marshaller;
-import org.exolab.castor.xml.Unmarshaller;
-import org.xml.sax.InputSource;
+import org.jibx.runtime.*;
+import org.jibx.runtime.impl.*;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
 
 /**
  * Class <code>XmlMapper</code> handles the marshalling and unmarshalling
  * of any entity with an external XML support. This is implemented on top
- * of Castor utility.
- *
- * @param <E> the precise class for entity to be handled
+ * of JiBX utility.
  *
  * @author Herv&eacute Bitteur
  * @version $Id$
  */
-public class XmlMapper <E>
+public class XmlMapper
 {
     //~ Static variables/initializers -------------------------------------
 
     private static final Logger logger = Logger.getLogger(XmlMapper.class);
 
+    /** Standard indentation value for xml formatting */
+    public static final int XML_INDENT = 4;
+
+    /** Standard encoding for xml file */
+     public static final String XML_ENCODING = "UTF-8";
+
     //~ Instance variables ------------------------------------------------
 
-    // The Castor mapping for loading and storing from/to an XML file
-    private final Mapping mapping;
+    // The JiBX factory for loading and storing from/to an XML file
+    private final IBindingFactory factory;
 
     //~ Constructors ------------------------------------------------------
 
@@ -49,33 +48,18 @@ public class XmlMapper <E>
     // XmlMapper //
     //-----------//
     /**
-     * Create an XML Mapper, based on its Castor descriptors
+     * Create an XML Mapper
      *
-     * @param mappingFileName the name of the file which contains the
-     *                        Castor mapping descriptors. <b>NOTA</b>The
-     *                        file is searched in the config directory of
-     *                        the application jar file, so a good example
-     *                        could be "/config/castor-glyph-mapping.xml"
+     * @param entityClass the class of the mapped entity
      */
-    public XmlMapper (String mappingFileName)
-            throws Exception
+    public XmlMapper (Class entityClass)
     {
         try {
-            mapping = new Mapping();
-            URL url = XmlMapper.class.getResource(mappingFileName);
-            mapping.loadMapping(url);
-        } catch (IOException ex) {
-            logger.warning("Cannot load mapping Xml file " + mappingFileName);
+            factory = BindingDirectory.getFactory(entityClass);
+        } catch (JiBXException ex) {
+            logger.warning("Cannot get a JiBX factory for " + entityClass);
             logger.warning(ex.toString());
-            throw ex;
-        } catch (MappingException ex) {
-            logger.warning("Cannot load mapping Xml file " + mappingFileName);
-            logger.warning(ex.toString());
-            throw ex;
-        } catch (NullPointerException ex) {
-            logger.warning("Cannot find mapping Xml file " + mappingFileName);
-            logger.warning(ex.toString());
-            throw ex;
+            throw new RuntimeException(ex);
         }
     }
 
@@ -85,22 +69,20 @@ public class XmlMapper <E>
     // load //
     //------//
     /**
-     * Unmarshal the provided XML file to allocate the corresponding entity
+     * Unmarshall the provided XML file to allocate the corresponding
+     * entity
      *
      * @param xmlFile the file that contains the Entity data in XML format
      *
      * @return the allocated entity or null if load has failed.
      */
-    @SuppressWarnings("unchecked")      // This inhibition does not work
-                                        // (Java 1.5 bug)
-        public E load (File xmlFile)
+    public Object load (File xmlFile)
         throws Exception
     {
         try {
-            Unmarshaller unmarshaller = new Unmarshaller(mapping);
-            // Unmarshal the data
-            Object obj = unmarshaller.unmarshal (new InputSource(new FileReader(xmlFile)));
-            E entity = (E) obj;         // Normal compiler warning here
+            IUnmarshallingContext uctx = factory.createUnmarshallingContext();
+            Object entity = uctx.unmarshalDocument
+                (new FileInputStream(xmlFile), null);
 
             if (logger.isDebugEnabled()) {
                 logger.debug("Entity loaded from " + xmlFile);
@@ -108,7 +90,7 @@ public class XmlMapper <E>
 
             return entity;
         } catch (Exception ex) {
-            logger.warning("Cannot Unmarshal from " + xmlFile);
+            logger.warning("Cannot Unmarshall from " + xmlFile);
             logger.warning(ex.toString());
 
             return null;
@@ -119,33 +101,27 @@ public class XmlMapper <E>
     // store //
     //-------//
     /**
-     * Marshal the provided Entity to its dedicated XML file
+     * Marshall the provided Entity to its dedicated XML file
      *
      * @param entity  the entity to be marshalled
      * @param xmlFile the XML file to be written
      */
-    public void store (E entity,
+    public void store (Object entity,
                        File xmlFile)
             throws Exception
     {
-        FileWriter writer = null;
         try {
-            // Marshal
-            writer = new FileWriter(xmlFile);
-            Marshaller marshaller = new Marshaller(writer);
-            marshaller.setMapping(mapping);
-            marshaller.marshal(entity);
+            IMarshallingContext mctx = factory.createMarshallingContext();
+            mctx.setIndent(XML_INDENT);
+            mctx.marshalDocument(entity, XML_ENCODING, null,
+                                 new FileOutputStream(xmlFile));
             if (logger.isDebugEnabled()) {
                 logger.debug("Entity written to " + xmlFile);
             }
         } catch (Exception ex) {
-            logger.warning("Cannot store entity to file " + xmlFile, ex);
+            logger.warning("Cannot marshall entity to file " + xmlFile);
+            logger.warning(ex.toString());
             throw ex;
-        } finally {
-            try {
-                writer.close();
-            } catch (Exception ex) {
-            }
         }
     }
 }
