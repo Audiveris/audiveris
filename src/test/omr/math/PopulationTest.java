7b1e742246a02c2c3a635e27ba08a4e057772206
hbitteur <herve.bitteur@audiveris.org>
2006-01-15 17:53:33 +0000

    Unit tests for Population of values

---
+++ src/test/omr/math/PopulationTest.java
@@ -0,0 +1,180 @@
+//--------------------------------------------------------------------------//
+//                                                                          //
+//                       P o p u l a t i o n T e s t                        //
+//                                                                          //
+//  Copyright (C) Herve Bitteur 2000-2005. All rights reserved.             //
+//  This software is released under the terms of the GNU General Public     //
+//  License. Please contact the author at herve.bitteur@laposte.net         //
+//  to report bugs & suggestions.                                           //
+//--------------------------------------------------------------------------//
+
+package omr.math;
+
+//import org.testng.annotations.*;
+import omr.util.BaseTestCase;
+
+import static junit.framework.Assert.*;
+import junit.framework.*;
+
+/**
+ * Class <code>PopulationTest</code> performs unit tests on Population
+ * class.
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public class PopulationTest
+    extends BaseTestCase
+{
+    //~ Static variables/initializers -------------------------------------
+
+    //~ Instance variables ------------------------------------------------
+
+    //~ Constructors ------------------------------------------------------
+
+    //~ Methods -----------------------------------------------------------
+
+    //-----------//
+    // testEmpty //
+    //-----------//
+    //@Test
+        public void testEmpty ()
+    {
+
+        Population p = new Population();
+
+        assertEquals("No values cumulated so far.",
+                     0, p.getCardinality());
+
+        try {
+            double mv = p.getMeanValue();
+            fail("Exception should be raised"+
+                 " when retrieving mean value of an empty population");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+
+        try {
+            double sd = p.getStandardDeviation();
+            fail("Exception should be raised"+
+                 " when retrieving standard deviation of an empty population");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+
+        try {
+            double v = p.getVariance();
+            fail("Exception should be raised"+
+                 " when retrieving variance of an empty population");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+
+        try {
+            p.excludeValue(123);
+            fail("Exception should be raised"+
+                 " when excluding a value from an empty population");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+    }
+
+    //~ -------------------------------------------------------------------
+
+    //---------------//
+    // testSingleton //
+    //---------------//
+    //@Test
+        public void testSingleton ()
+    {
+
+        Population p = new Population();
+        double val = 123d;
+        p.includeValue(val);
+
+        assertEquals("Population should contain one value.",
+                     1, p.getCardinality());
+
+        assertEquals("Check mean value.",
+                     val, p.getMeanValue());
+
+        try {
+            double sd = p.getStandardDeviation();
+            fail("Exception should be raised"+
+                 " when retrieving standard deviation of a singleton");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+
+        try {
+            double v = p.getVariance();
+            fail("Exception should be raised"+
+                 " when retrieving variance of a singleton");
+        } catch (Exception expected) {
+            checkException(expected);
+        }
+
+        p.excludeValue(val);
+        assertEquals("Population should contain no value.",
+                     0, p.getCardinality());
+    }
+
+    //-------------//
+    // testInclude //
+    //-------------//
+    //@Test
+        public void testInclude ()
+    {
+
+        Population p = new Population();
+        p.includeValue(5);
+        p.includeValue(6);
+        p.includeValue(8);
+        p.includeValue(9);
+
+        assertEquals("Population should contain 4 values.",
+                     4, p.getCardinality());
+
+        assertEquals("Check mean value.",
+                     7d, p.getMeanValue());
+
+        double v = p.getVariance();
+        assertEquals("Check variance of 4 values.",
+                     10d/3, p.getVariance());
+
+        double sd = p.getStandardDeviation();
+        assertNears("Check standard deviation of 4 values.",
+                    Math.sqrt(10d/3), p.getStandardDeviation());
+    }
+
+    //-------------//
+    // testExclude //
+    //-------------//
+    //@Test
+        public void testExclude ()
+    {
+
+        Population p = new Population();
+        p.includeValue(5);
+        p.includeValue(6);
+        p.includeValue(8);
+        p.includeValue(9);
+
+        p.excludeValue(5);
+        p.excludeValue(9);
+
+        assertEquals("Population should contain 2 values.",
+                     2, p.getCardinality());
+
+        assertEquals("Check mean value.",
+                     7d, p.getMeanValue());
+
+        double v = p.getVariance();
+        assertEquals("Check variance of 2 values.",
+                     2d, p.getVariance());
+
+        double sd = p.getStandardDeviation();
+        assertNears("Check standard deviation of 2 values.",
+                    Math.sqrt(2d), p.getStandardDeviation());
+    }
+}
