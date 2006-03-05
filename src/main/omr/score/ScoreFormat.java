9d399a02e794930af7ea5f471390b3922d35abfd
hbitteur <herve.bitteur@audiveris.org>
2006-03-05 18:45:48 +0000

    Enum type to handle various formats for storing scores

---
+++ src/main/omr/score/ScoreFormat.java
@@ -0,0 +1,73 @@
+//--------------------------------------------------------------------------//
+//                                                                          //
+//                          S c o r e F o r m a t                           //
+//                                                                          //
+//  Copyright (C) Herve Bitteur 2000-2005. All rights reserved.             //
+//  This software is released under the terms of the GNU General Public     //
+//  License. Please contact the author at herve.bitteur@laposte.net         //
+//  to report bugs & suggestions.                                           //
+//--------------------------------------------------------------------------//
+
+package omr.score;
+
+import omr.constant.Constant;
+import omr.constant.ConstantSet;
+
+/**
+ * Class <code>ScoreFormat</code> defines an enumeration of all possible
+ * formats for score files.
+ */
+public enum ScoreFormat
+{
+    //~ Static variables/initializers -------------------------------------
+
+    /**
+     * Binary format, used with plain Java (de)serialization
+     */
+    BINARY("Binary",
+           ".score"),
+
+    /**
+     * XML ASCII format, used with an XML mapper
+     */
+    XML("Xml",
+        ".xml");
+
+    //~ Instance variables ------------------------------------------------
+
+    /**
+     * Readable name of the format
+     */
+    public final String name;
+
+    /**
+     * File extension used for file name
+     */
+    public final String extension;
+
+    public final Constant.String folder;
+
+    //~ Constructors ------------------------------------------------------
+
+    //-------------//
+    // ScoreFormat //
+    //-------------//
+    /**
+     * Definition of a score format
+     *
+     * @param name user name for the format
+     * @param extension related file extension
+     */
+    ScoreFormat (String name,
+                 String extension)
+    {
+        this.name      = name;
+        this.extension = extension;
+
+        folder = new Constant.String
+            (getClass().toString(),
+             "score" + name + "Folder",
+             "c:/",
+             "Default directory for " + name + " score files");
+    }
+}
