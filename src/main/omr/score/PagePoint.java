b20cee84ae9b974ebf0227ff4fd495f86913b83b
hbitteur <herve.bitteur@audiveris.org>
2006-04-12 14:31:37 +0000

    Added specific constructors

--- src/main/omr/score/PagePoint.java
+++ src/main/omr/score/PagePoint.java
@@ -14,9 +14,12 @@ import java.awt.*;
 
 /**
  * Class <code>PagePoint</code> is a simple Point that is meant to
- * represent a point in a virtual page. This specialization is used to take
- * benefit of compiler checks, to prevent the use of points with incorrect
- * meaning or units.
+ * represent a point in a virtual page, where Systems are arranged one
+ * under the other as in a normal Sheet display, and where coordinates are
+ * expressed in units.
+ *
+ * <p>This specialization is used to take benefit of compiler checks, to
+ * prevent the use of points with incorrect meaning or units.
  *
  * @author Herv&eacute; Bitteur
  * @version $Id$
@@ -24,4 +27,19 @@ import java.awt.*;
 public class PagePoint
     extends Point
 {
+    //-----------//
+    // PagePoint //
+    //-----------//
+    public PagePoint ()
+    {
+    }
+
+    //-----------//
+    // PagePoint //
+    //-----------//
+    public PagePoint (int x,
+                      int y)
+    {
+        super(x, y);
+    }
 }
