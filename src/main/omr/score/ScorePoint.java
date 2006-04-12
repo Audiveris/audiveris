d77d3565e7e645e2bba903c89b1d9b4789d30795
hbitteur <herve.bitteur@audiveris.org>
2006-04-12 14:36:31 +0000

    A point in the (horizontal) Score display

---
+++ src/main/omr/score/ScorePoint.java
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------//
+//                                                                       //
+//                          S c o r e P o i n t                          //
+//                                                                       //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.          //
+//  This software is released under the terms of the GNU General Public  //
+//  License. Please contact the author at herve.bitteur@laposte.net      //
+//  to report bugs & suggestions.                                        //
+//-----------------------------------------------------------------------//
+
+package omr.score;
+
+import java.awt.*;
+
+/**
+ * Class <code>ScorePoint</code> is a simple Point that is meant to
+ * represent a point in the Score display, where Systems are arranged
+ * horizontally one after the other, and where coordinates are expressed in
+ * units.
+ *
+ * <p>This specialization is used to take benefit of compiler checks, to
+ * prevent the use of points with incorrect meaning or units.
+ *
+ * @author Herv&eacute; Bitteur
+ * @version $Id$
+ */
+public class ScorePoint
+    extends Point
+{
+}
