b03a9121384075737fbea6a8887be771b23b5fcd
hbitteur <herve.bitteur@audiveris.org>
2006-09-18 14:08:21 +0000

    curtomer-annotation to flag an implementing method

---
+++ src/main/omr/util/Implement.java
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                             I m p l e m e n t                              //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation <code>Implement</code> is used to flag a method which (is part of)
+ * the implementation of an interface
+ */
+@Documented
+@Target(ElementType.METHOD)
+public @interface Implement
+{
+    /** Interface whose method is implemented */
+    Class value();
+}
