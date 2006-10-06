3ccdf3a856f9d1a124348819145d9468439741a7
hbitteur <herve.bitteur@audiveris.org>
2006-10-06 08:25:39 +0000

    Unit test for unmarshalling & marshalling a SymbolIcon entity

---
+++ src/test/omr/ui/icon/IconTest.java
@@ -0,0 +1,112 @@
+//-----------------------------------------------------------------------//
+//                                                                       //
+//                            I c o n T e s t                            //
+//                                                                       //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.          //
+//  This software is released under the terms of the GNU General Public  //
+//  License. Please contact the author at herve.bitteur@laposte.net      //
+//  to report bugs & suggestions.                                        //
+//-----------------------------------------------------------------------//
+package omr.ui.icon;
+
+import omr.glyph.Shape;
+
+import omr.util.BaseTestCase;
+import omr.util.Dumper;
+import omr.util.XmlMapper;
+import static junit.framework.Assert.*;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.io.*;
+
+import javax.swing.ImageIcon;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Herv&eacute; Bitteur
+ * @version $Id$
+ */
+public class IconTest
+    extends BaseTestCase
+{
+    //~ Methods ----------------------------------------------------------------
+
+    //------//
+    // main //
+    //------//
+    public static void main (String... args)
+    {
+        new IconTest().play(args[0]);
+    }
+
+    //---------//
+    // convert //
+    //---------//
+//    public void convert ()
+//    {
+//        // Convert icon files (raw -> xml)
+//        for (Shape shape : Shape.values()) {
+//            SymbolIcon icon = (SymbolIcon) shape.getIcon();
+//
+//            if (icon != null) {
+//                System.out.println("Processing " + shape);
+//                IconManager.getInstance()
+//                           .storeSymbolIcon((SymbolIcon) shape.getIcon());
+//            } else {
+//                System.out.println("***Skipping " + shape);
+//            }
+//        }
+//    }
+
+    //------//
+    // play //
+    //------//
+    public void play (String fileName)
+    {
+        try {
+            SymbolIcon icon = IconManager.getInstance()
+                                         .loadFromXmlStream(
+                new FileInputStream(new File(fileName)));
+
+            Dumper.dump(icon);
+
+            IconManager.getInstance()
+                       .storeToXmlStream(
+                icon,
+                new FileOutputStream(new File(fileName + ".out.xml")));
+            System.out.println("Store done.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //-----------//
+    // playBasic //
+    //-----------//
+    public void playBasic (String fileName)
+    {
+        try {
+            XmlMapper  mapper = new XmlMapper(SymbolIcon.class);
+            SymbolIcon icon = (SymbolIcon) mapper.load(new File(fileName));
+
+            Dumper.dump(icon);
+
+            mapper.store(icon, new File(fileName + ".out.xml"));
+            System.out.println("Store done.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //--------------//
+    // testMarshall //
+    //--------------//
+    public void testMarshall ()
+    {
+        play("/soft/audiveris/src/test/omr/ui/icon/icon-data.xml");
+
+        //convert();
+    }
+}
