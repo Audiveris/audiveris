2758cedf4f0d8fb43437441fcfb16cac1a1cb44b
hbitteur <herve.bitteur@audiveris.org>
2006-10-13 10:07:14 +0000

    Added javadoc warning

--- src/main/omr/score/visitor/Visitable.java
+++ src/main/omr/score/visitor/Visitable.java
@@ -29,7 +29,9 @@ public interface Visitable
      * General entry for any visiting
      *
      * @param visitor concrete visitor object to define the actual processing
-     * @return true if children must be visited also, false otherwise
+     * @return true if children must be visited also, false otherwise.
+     *              <b>Nota</b>: Unless there is a compelling reason, it's safer
+     *              to return true to let the visitor work normally.
      */
     boolean accept (Visitor visitor);
 }
