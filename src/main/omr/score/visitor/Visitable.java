b6cdca780f7610141405891a13836f89165306ca
hbitteur <herve.bitteur@audiveris.org>
2006-10-13 09:48:17 +0000

    Any MusicNode must be Visitable by a score visitor

---
+++ src/main/omr/score/visitor/Visitable.java
@@ -0,0 +1,36 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                             V i s i t a b l e                              //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.score.visitor;
+
+import omr.score.*;
+
+/**
+ * Interface <code>Visitable</code> must be implemented by any node to be
+ * visited in the Score hierarchy
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public interface Visitable
+{
+    //~ Methods ----------------------------------------------------------------
+
+    //--------//
+    // accept //
+    //--------//
+    /**
+     * General entry for any visiting
+     *
+     * @param visitor concrete visitor object to define the actual processing
+     * @return true if children mast be visited also, false otherwise
+     */
+    boolean accept (Visitor visitor);
+}
