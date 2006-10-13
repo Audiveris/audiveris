b501cf8741da1b81e2b17d60a18afc50e26b58e3
hbitteur <herve.bitteur@audiveris.org>
2006-10-13 15:57:21 +0000

    A rectangle in pixels

---
+++ src/main/omr/sheet/PixelRectangle.java
@@ -0,0 +1,61 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                        P i x e l R e c t a n g l e                         //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.sheet;
+
+import java.awt.*;
+
+/**
+ * Class <code>PixelRectangle</code> is a simple Rectangle that is meant to
+ * represent a rectangle in a sheet, with its components specified in pixels, so
+ * the name.
+ *
+ * <p> This specialization is used to take benefit of compiler checks, to
+ * prevent the use of rectangles with incorrect meaning or units. </p>
+ *
+ * @author Herv&eacute; Bitteur
+ * @version $Id$
+ */
+public class PixelRectangle
+    extends Rectangle
+{
+    //~ Constructors -----------------------------------------------------------
+
+    //----------------//
+    // PixelRectangle //
+    //----------------//
+    /**
+     * Creates an instance of <code>PixelRectangle</code> with all data set to
+     * zero.
+     */
+    public PixelRectangle ()
+    {
+    }
+
+    //----------------//
+    // PixelRectangle //
+    //----------------//
+    /**
+     * Construct a <code>PixelRectangle</code> and initialize it with the
+     * specified data
+     *
+     * @param x the specified x
+     * @param y the specified y
+     * @param width the specified width
+     * @param height the specified height
+     */
+    public PixelRectangle (int x,
+                           int y,
+                           int width,
+                           int height)
+    {
+        super(x, y, width, height);
+    }
+}
