bf0b3ab00a4149374c2f788a198755a493552c6e
hbitteur <herve.bitteur@audiveris.org>
2006-10-26 11:53:45 +0000

    Info for JAXB use within the score package

---
+++ src/main/omr/score/package-info.java
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                          p a c k a g e - i n f o                           //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+@XmlAccessorType(FIELD)
+package omr.score;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
