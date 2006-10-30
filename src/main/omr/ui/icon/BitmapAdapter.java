32fa78e80c1e8eea40a8760a9f0e6799179b128d
hbitteur <herve.bitteur@audiveris.org>
2006-10-30 21:45:35 +0000

    Temporary attempt for marshalling bitmaps

---
+++ src/main/omr/ui/icon/BitmapAdapter.java
@@ -0,0 +1,63 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                         B i t m a p A d a p t e r                          //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.ui.icon;
+
+import java.awt.image.BufferedImage;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/*
+ *
+ *  PurchaseList - ValueType
+ *  HashMap - BoundType
+ */
+public class BitmapAdapter
+    extends XmlAdapter<String[], BufferedImage>
+{
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new BitmapAdapter object.
+     */
+    public BitmapAdapter ()
+    {
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //---------//
+    // marshal //
+    //---------//
+    /**
+     * Convert a bound type to a value type.
+     * write Java content into class that generates desired XML
+     */
+    public String[] marshal (BufferedImage image)
+    {
+        System.out.println("BA marshal");
+        return IconManager.getInstance()
+                          .encodeImage(image);
+    }
+
+    //-----------//
+    // unmarshal //
+    //-----------//
+    /**
+     * Convert a value type to a bound type.
+     * read xml content and put into Java class.
+     */
+    public BufferedImage unmarshal (String[] rows)
+    {
+        System.out.println("BA unmarshal rows.length=" + rows.length);
+        return IconManager.getInstance()
+                          .decodeImage(rows);
+    }
+}
