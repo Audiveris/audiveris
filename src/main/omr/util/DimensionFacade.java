09f567a39654386915644a794e69920db0c54cbc
hbitteur <herve.bitteur@audiveris.org>
2006-10-30 21:42:10 +0000

    Temporary facade for JAXB marshalling of JRE classes

---
+++ src/main/omr/util/DimensionFacade.java
@@ -0,0 +1,107 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                       D i m e n s i o n F a c a d e                        //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.util;
+
+import java.awt.Dimension;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * Class <code>DimensionFacade</code> is a (hopefully temporary) fix to allow Xml
+ * binding of standard class Dimension that we cannot annotate
+ *
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "dimension")
+public class DimensionFacade
+{
+    //~ Instance fields --------------------------------------------------------
+
+    /** The interfaced Dimension instance */
+    private Dimension dimension;
+
+    //~ Constructors -----------------------------------------------------------
+
+    //-----------------//
+    // DimensionFacade //
+    //-----------------//
+    /**
+     * Creates a new instance of DimensionFacade
+     */
+    public DimensionFacade ()
+    {
+    }
+
+    //-----------------//
+    // DimensionFacade //
+    //-----------------//
+    /**
+     * Creates a new DimensionFacade object.
+     *
+     * @param dimension the interfaced dimension
+     */
+    public DimensionFacade (Dimension dimension)
+    {
+        this.dimension = dimension;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //--------------//
+    // getDimension //
+    //--------------//
+    /**
+     * Report the interfaced dimension
+     *
+     * @return the actual dimension
+     */
+    public Dimension getDimension ()
+    {
+        return dimension;
+    }
+
+    //----------//
+    // setWidth //
+    //----------//
+    @XmlElement
+    public void setWidth (int width)
+    {
+        dimension.width = width;
+    }
+
+    //----------//
+    // getWidth //
+    //----------//
+    public int getWidth ()
+    {
+        return dimension.width;
+    }
+
+    //-----------//
+    // setHeight //
+    //-----------//
+    @XmlElement
+    public void setHeight (int height)
+    {
+        dimension.height = height;
+    }
+
+    //-----------//
+    // getHeight //
+    //-----------//
+    public int getHeight ()
+    {
+        return dimension.height;
+    }
+}
---
+++ src/main/omr/util/PointFacade.java
@@ -0,0 +1,107 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                           P o i n t F a c a d e                            //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.util;
+
+import java.awt.Point;
+
+import javax.xml.bind.annotation.*;
+
+/**
+ * Class <code>PointFacade</code> is a (hopefully temporary) fix to allow Xml
+ * binding of standard class Point that we cannot annotate
+ *
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "point")
+public class PointFacade
+{
+    //~ Instance fields --------------------------------------------------------
+
+    /** The interfaced Point instance */
+    private Point point;
+
+    //~ Constructors -----------------------------------------------------------
+
+    //-------------//
+    // PointFacade //
+    //-------------//
+    /**
+     * Creates a new instance of PointFacade
+     */
+    public PointFacade ()
+    {
+    }
+
+    //-------------//
+    // PointFacade //
+    //-------------//
+    /**
+     * Creates a new PointFacade object.
+     *
+     * @param point the interfaced point
+     */
+    public PointFacade (Point point)
+    {
+        this.point = point;
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //----------//
+    // getPoint //
+    //----------//
+    /**
+     * Report the interfaced point
+     *
+     * @return the actual point
+     */
+    public Point getPoint ()
+    {
+        return point;
+    }
+
+    //------//
+    // setX //
+    //------//
+    @XmlElement
+    public void setX (int x)
+    {
+        point.x = x;
+    }
+
+    //------//
+    // getX //
+    //------//
+    public int getX ()
+    {
+        return point.x;
+    }
+
+    //------//
+    // setY //
+    //------//
+    @XmlElement
+    public void setY (int y)
+    {
+        point.y = y;
+    }
+
+    //------//
+    // getY //
+    //------//
+    public int getY ()
+    {
+        return point.y;
+    }
+}
