0eb0b4563537d87871726e069603444a77d69256
hbitteur <herve.bitteur@audiveris.org>
2006-11-02 15:16:15 +0000

    Stand-alone version of former StickArea.Source

---
+++ src/main/omr/stick/SticksSource.java
@@ -0,0 +1,151 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                          S t i c k s S o u r c e                           //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.stick;
+
+import omr.glyph.GlyphSection;
+
+import omr.stick.SticksBuilder.SectionPredicate;
+
+import java.util.*;
+
+/**
+ * Class <code>Source</code> allows to formalize the way relevant sections are
+ * made available to the area to be scanned.
+ *
+ * @author Herv&eacute; Bitteur
+ * @version $Id$
+ */
+public class SticksSource
+{
+    //~ Instance fields --------------------------------------------------------
+
+    /** the predicate to check whether section is to be processed */
+    protected final SectionPredicate predicate;
+
+    /** the section iterator for the source */
+    protected ListIterator<GlyphSection> vi;
+
+    /** the section currently visited */
+    protected GlyphSection section;
+
+    //~ Constructors -----------------------------------------------------------
+
+    //--------------//
+    // SticksSource //
+    //--------------//
+    /**
+     * Create a SticksBuilder source on a given collection of glyph sections,
+     * with default predicate
+     *
+     * @param collection the provided sections
+     */
+    public SticksSource (Collection<GlyphSection> collection)
+    {
+        this(collection, new SectionPredicate());
+    }
+
+    //--------------//
+    // SticksSource //
+    //--------------//
+    /**
+     * Create a SticksBuilder source on a given collection of glyph sections,
+     * with a specific predicate for section
+     *
+     * @param collection the provided sections
+     * @param predicate the predicate to check for candidate sections
+     */
+    public SticksSource (Collection<GlyphSection> collection,
+                         SectionPredicate         predicate)
+    {
+        this.predicate = predicate;
+
+        if (collection != null) {
+            ArrayList<GlyphSection> list = new ArrayList<GlyphSection>(
+                collection);
+            vi = list.listIterator();
+        }
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //----------//
+    // isInArea //
+    //----------//
+    /**
+     * Check whether a given section lies entirely within the scanned area
+     *
+     * @param section The section to be checked
+     *
+     * @return The boolean result of the test
+     */
+    public boolean isInArea (GlyphSection section)
+    {
+        // Default behavior : no filtering
+        return true;
+    }
+
+    //--------//
+    // backup //
+    //--------//
+    public void backup ()
+    {
+        // void
+    }
+
+    //---------//
+    // hasNext //
+    //---------//
+    /**
+     * Check whether we have more sections to scan
+     *
+     * @return the boolean result of the test
+     */
+    public boolean hasNext ()
+    {
+        while (vi.hasNext()) {
+            // Update cached data
+            section = (StickSection) vi.next();
+
+            if (predicate.check(section)) {
+                if (section instanceof StickSection) {
+                    ((StickSection) section).role = null; // Safer ?
+                }
+
+                section.setGlyph(null); // Safer ?
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //------//
+    // next //
+    //------//
+    /**
+     * Return the next relevant section in Area, if any
+     *
+     * @return the next section
+     */
+    public GlyphSection next ()
+    {
+        return section;
+    }
+
+    //-------//
+    // reset //
+    //-------//
+    public void reset ()
+    {
+        // void
+    }
+}
