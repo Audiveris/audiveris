562a0ee37108b239305eb5d054bbfd7a63939920
hbitteur <herve.bitteur@audiveris.org>
2006-11-10 08:24:36 +0000

    Definition of a standalone Note entity, either a real note or a rest

---
+++ src/main/omr/score/Note.java
@@ -0,0 +1,62 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                                  N o t e                                   //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.score;
+
+import omr.glyph.Glyph;
+import omr.glyph.Shape;
+
+import omr.score.visitor.Visitor;
+
+/**
+ * Class <code>Note</code>
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public class Note
+    extends StaffNode
+{
+    //~ Instance fields --------------------------------------------------------
+
+    /** The note shape */
+    private Shape shape;
+
+    /** Accidental is any */
+    private Shape accidental;
+
+    /** Pitch position */
+    private double pitchPosition;
+
+    /** The underlying glyph */
+    private Glyph glyph;
+
+    //~ Constructors -----------------------------------------------------------
+
+    //------//
+    // Note //
+    //------//
+    /** Creates a new instance of Note */
+    public Note (StaffNode container)
+    {
+        super(container, container.getStaff());
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //--------//
+    // accept //
+    //--------//
+    @Override
+    public boolean accept (Visitor visitor)
+    {
+        return visitor.visit(this);
+    }
+}
