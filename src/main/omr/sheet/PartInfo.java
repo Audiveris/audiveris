034454fdd204836dd9c789e38650b3533f2e3d77
hbitteur <herve.bitteur@audiveris.org>
2006-11-19 19:18:10 +0000

    Physical definition of a part (sequence of staves) in a SystemInfo

---
+++ src/main/omr/sheet/PartInfo.java
@@ -0,0 +1,57 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                              P a r t I n f o                               //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.sheet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class <code>PartInfo</code> is the physical gathering of StaffInfo instances
+ * in a part
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public class PartInfo
+{
+    //~ Instance fields --------------------------------------------------------
+
+    /** Staves in this part */
+    private List<StaffInfo> staves = new ArrayList<StaffInfo>();
+
+    //~ Constructors -----------------------------------------------------------
+
+    //----------//
+    // PartInfo //
+    //----------//
+    /** Creates a new instance of PartInfo */
+    public PartInfo ()
+    {
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //-----------//
+    // getStaves //
+    //-----------//
+    public List<StaffInfo> getStaves ()
+    {
+        return staves;
+    }
+
+    //----------//
+    // addStaff //
+    //----------//
+    public void addStaff (StaffInfo staffInfo)
+    {
+        staves.add(staffInfo);
+    }
+}
