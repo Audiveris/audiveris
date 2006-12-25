c6ce9286a1a0d7df2749f8aa2db38080dc652906
hbitteur <herve.bitteur@audiveris.org>
2006-12-25 23:22:37 +0000

    Use of ScoreVisitor

--- src/main/omr/score/visitor/Visitable.java
+++ src/main/omr/score/visitor/Visitable.java
@@ -33,5 +33,5 @@ public interface Visitable
      *              <b>Nota</b>: Unless there is a compelling reason, it's safer
      *              to return true to let the visitor work normally.
      */
-    boolean accept (Visitor visitor);
+    boolean accept (ScoreVisitor visitor);
 }
