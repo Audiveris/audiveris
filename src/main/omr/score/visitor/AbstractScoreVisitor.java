1279083c2cd538e2d06e7e15d79d75231aaf22ad
hbitteur <herve.bitteur@audiveris.org>
2006-12-25 23:19:39 +0000

    Basis for implementing a score visitor

---
+++ src/main/omr/score/visitor/AbstractScoreVisitor.java
@@ -0,0 +1,185 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                  A b s t r a c t S c o r e V i s i t o r                   //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2006. All rights reserved.               //
+//  This software is released under the terms of the GNU General Public       //
+//  License. Please contact the author at herve.bitteur@laposte.net           //
+//  to report bugs & suggestions.                                             //
+//----------------------------------------------------------------------------//
+//
+package omr.score.visitor;
+
+import omr.score.Barline;
+import omr.score.Beam;
+import omr.score.Chord;
+import omr.score.Clef;
+import omr.score.KeySignature;
+import omr.score.Measure;
+import omr.score.MeasureNode;
+import omr.score.Note;
+import omr.score.PartNode;
+import omr.score.Score;
+import omr.score.ScoreNode;
+import omr.score.ScorePart;
+import omr.score.Slur;
+import omr.score.Staff;
+import omr.score.System;
+import omr.score.SystemPart;
+import omr.score.TimeSignature;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Class <code>AbstractScoreVisitor</code> provides a basic implementation of
+ * the ScoreVisitor interface
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public class AbstractScoreVisitor
+    implements ScoreVisitor
+{
+    //~ Constructors -----------------------------------------------------------
+
+    //----------------------//
+    // AbstractScoreVisitor //
+    //----------------------//
+    /**
+     * Creates a new AbstractScoreVisitor object.
+     */
+    public AbstractScoreVisitor ()
+    {
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //---------------//
+    // visit Barline //
+    //---------------//
+    public boolean visit (Barline barline)
+    {
+        return true;
+    }
+
+    //------------//
+    // visit Beam //
+    //------------//
+    public boolean visit (Beam beam)
+    {
+        return true;
+    }
+
+    //-------------//
+    // visit Chord //
+    //-------------//
+    public boolean visit (Chord chord)
+    {
+        return true;
+    }
+
+    //------------//
+    // visit Clef //
+    //------------//
+    public boolean visit (Clef clef)
+    {
+        return true;
+    }
+
+    //--------------------//
+    // visit KeySignature //
+    //--------------------//
+    public boolean visit (KeySignature keySignature)
+    {
+        return true;
+    }
+
+    //---------------//
+    // visit Measure //
+    //---------------//
+    public boolean visit (Measure measure)
+    {
+        return true;
+    }
+
+    //-------------------//
+    // visit MeasureNode //
+    //-------------------//
+    public boolean visit (MeasureNode measureNode)
+    {
+        return true;
+    }
+
+    //------------//
+    // visit Note //
+    //------------//
+    public boolean visit (Note note)
+    {
+        return true;
+    }
+
+    //----------------//
+    // visit PartNode //
+    //----------------//
+    public boolean visit (PartNode partNode)
+    {
+        return true;
+    }
+
+    //-----------------//
+    // visit ScoreNode //
+    //-----------------//
+    public boolean visit (ScoreNode scoreNode)
+    {
+        return true;
+    }
+
+    //-------------//
+    // visit Score //
+    //-------------//
+    public boolean visit (Score score)
+    {
+        return true;
+    }
+
+    //------------//
+    // visit Slur //
+    //------------//
+    public boolean visit (Slur slur)
+    {
+        return true;
+    }
+
+    //-------------//
+    // visit Staff //
+    //-------------//
+    public boolean visit (Staff staff)
+    {
+        return true;
+    }
+
+    //--------------//
+    // visit System //
+    //--------------//
+    public boolean visit (System system)
+    {
+        return true;
+    }
+
+    //------------------//
+    // visit SystemPart //
+    //------------------//
+    public boolean visit (SystemPart systemPart)
+    {
+        return true;
+    }
+
+    //---------------------//
+    // visit TimeSignature //
+    //---------------------//
+    public boolean visit (TimeSignature timeSignature)
+    {
+        return true;
+    }
+}
