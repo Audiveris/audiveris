02e2ae1d07d6922c4c025250a66820f5fc2ff096
hbitteur <herve.bitteur@audiveris.org>
2007-03-28 21:22:43 +0000

    Enhanced handling of all dynamics symbols

--- src/main/omr/score/Dynamics.java
+++ src/main/omr/score/Dynamics.java
@@ -46,30 +46,65 @@ public class Dynamics
     private static final Map<Shape, String> sigs = new HashMap<Shape, String>();
 
     static {
-        sigs.put(Shape.PIANISSISSIMO, "ppp");
-        sigs.put(Shape.PIANISSIMO, "pp");
-        sigs.put(Shape.PIANO, "p");
-        sigs.put(Shape.MEZZO_PIANO, "mp");
-        sigs.put(Shape.MEZZO, "m");
-        sigs.put(Shape.MEZZO_FORTE, "mf");
-        sigs.put(Shape.FORTE, "f");
-        sigs.put(Shape.FORTISSIMO, "ff");
-        sigs.put(Shape.FORTISSISSIMO, "fff");
+        // Additional characters : m, r, s & z
+        sigs.put(Shape.DYNAMICS_CHAR_M, "m");
+        sigs.put(Shape.DYNAMICS_CHAR_R, "r");
+        sigs.put(Shape.DYNAMICS_CHAR_S, "s");
+        sigs.put(Shape.DYNAMICS_CHAR_Z, "z");
+        //
+        // True dynamics symbols
+        sigs.put(Shape.DYNAMICS_F, "f");
+        sigs.put(Shape.DYNAMICS_FF, "ff");
+        sigs.put(Shape.DYNAMICS_FFF, "fff");
+        sigs.put(Shape.DYNAMICS_FFFF, "ffff");
+        sigs.put(Shape.DYNAMICS_FFFFF, "fffff");
+        sigs.put(Shape.DYNAMICS_FFFFFF, "ffffff");
+        sigs.put(Shape.DYNAMICS_FP, "fp");
+        sigs.put(Shape.DYNAMICS_FZ, "fz");
+        sigs.put(Shape.DYNAMICS_MF, "mf");
+        sigs.put(Shape.DYNAMICS_MP, "mp");
+        sigs.put(Shape.DYNAMICS_P, "p");
+        sigs.put(Shape.DYNAMICS_PP, "pp");
+        sigs.put(Shape.DYNAMICS_PPP, "ppp");
+        sigs.put(Shape.DYNAMICS_PPPP, "pppp");
+        sigs.put(Shape.DYNAMICS_PPPPP, "ppppp");
+        sigs.put(Shape.DYNAMICS_PPPPPP, "pppppp");
+        sigs.put(Shape.DYNAMICS_RF, "rf");
+        sigs.put(Shape.DYNAMICS_RFZ, "rfz");
+        sigs.put(Shape.DYNAMICS_SF, "sf");
+        sigs.put(Shape.DYNAMICS_SFFZ, "sffz");
+        sigs.put(Shape.DYNAMICS_SFP, "sfp");
+        sigs.put(Shape.DYNAMICS_SFPP, "sfpp");
+        sigs.put(Shape.DYNAMICS_SFZ, "sfz");
     }
 
     /* Map Signature -> Shape */
     private static final Map<String, Shape> shapes = new HashMap<String, Shape>();
 
     static {
-        shapes.put("ppp", Shape.PIANISSISSIMO);
-        shapes.put("pp", Shape.PIANISSIMO);
-        shapes.put("p", Shape.PIANO);
-        shapes.put("mp", Shape.MEZZO_PIANO);
-        shapes.put("m", Shape.MEZZO);
-        shapes.put("mf", Shape.MEZZO_FORTE);
-        shapes.put("f", Shape.FORTE);
-        shapes.put("ff", Shape.FORTISSIMO);
-        shapes.put("fff", Shape.FORTISSISSIMO);
+        shapes.put("f", Shape.DYNAMICS_F);
+        shapes.put("ff", Shape.DYNAMICS_FF);
+        shapes.put("fff", Shape.DYNAMICS_FFF);
+        shapes.put("ffff", Shape.DYNAMICS_FFFF);
+        shapes.put("fffff", Shape.DYNAMICS_FFFFF);
+        shapes.put("ffffff", Shape.DYNAMICS_FFFFFF);
+        shapes.put("fp", Shape.DYNAMICS_FP);
+        shapes.put("fz", Shape.DYNAMICS_FZ);
+        shapes.put("mf", Shape.DYNAMICS_MF);
+        shapes.put("mp", Shape.DYNAMICS_MP);
+        shapes.put("p", Shape.DYNAMICS_P);
+        shapes.put("pp", Shape.DYNAMICS_PP);
+        shapes.put("ppp", Shape.DYNAMICS_PPP);
+        shapes.put("pppp", Shape.DYNAMICS_PPPP);
+        shapes.put("ppppp", Shape.DYNAMICS_PPPPP);
+        shapes.put("pppppp", Shape.DYNAMICS_PPPPPP);
+        shapes.put("rf", Shape.DYNAMICS_RF);
+        shapes.put("rfz", Shape.DYNAMICS_RFZ);
+        shapes.put("sf", Shape.DYNAMICS_SF);
+        shapes.put("sffz", Shape.DYNAMICS_SFFZ);
+        shapes.put("sfp", Shape.DYNAMICS_SFP);
+        shapes.put("sfpp", Shape.DYNAMICS_SFPP);
+        shapes.put("sfz", Shape.DYNAMICS_SFZ);
     }
 
     //~ Constructors -----------------------------------------------------------
@@ -177,14 +212,21 @@ public class Dynamics
     //------------------//
     private boolean isCompatibleWith (SystemPoint point)
     {
-        // Check x-proximity and y-alignment (TBI)
+        // Check x-proximity and y-alignment 
         Scale scale = getSystem()
                           .getScale();
         int   dx = scale.toUnits(constants.maxDx);
         int   dy = scale.toUnits(constants.maxDy);
 
-        return (Math.abs(getPoint().x - point.x) <= dx) &&
-               (Math.abs(getPoint().y - point.y) <= dy);
+        // Horizontal distance
+        int xDist = Math.min(
+            Math.abs(getBox().x - point.x),
+            Math.abs((getBox().x + getBox().width) - point.x));
+
+        // Vertical distance
+        int yDist = Math.abs(getPoint().y - point.y);
+
+        return (xDist <= dx) && (yDist <= dy);
     }
 
     //~ Inner Classes ----------------------------------------------------------
