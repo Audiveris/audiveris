7436f0dab9c35228f632bb2f8403523d0a8ce124
hbitteur <herve.bitteur@audiveris.org>
2007-07-07 16:27:21 +0000

    A first set of modifications for 3.0

--- www/docs/manual/index.js
+++ www/docs/manual/index.js
@@ -10,22 +10,39 @@
 //      $Id$
 
 // Table of defined manuals, add any new manual to this list
-var manuals = new Array("releases", "installation", "operation", "design", "implementation", "api");
+var manuals = new Array("releases", "installation", "operation", "design", "implementation", "notes", "api");
+
+// Must be parallel to previous table
+var manualNames = new Array("Releases", "Installation", "Operation", "Design", "Implementation", "Notes", "API-javadoc");
 
 // Selected Manual
-selectedManual = "";
+top.selectedManual = "";
 
 // Select a manual by its name
 function selectManual (manual)
 {
-    selectedManual = manual;
-    updateRow(manual);
+    // Default value for manual
+    if (manual == "") {
+       manual = manuals[0];
+    }
+    
+    // Remember the selected manual
+    top.selectedManual = manual;
+}
+
+// Actually load the manual and its toc into the proper frames
+function loadManual(manual)
+{
     if (manual != "") {
         if (manual == "api") {
             // Load the API index in Pack frameset
-            parent.Pack.location.href  = "../api/index.html";
-        } else {
-            parent.Pack.location.href  = "../manual/index-pack.html";
+            Pack.location.href  = "../api/index.html";
+        } else if (manual == "notes") {
+            // Load the tiddly wiki in Pack frameset
+            Pack.location.href  = "../wiki/notebook.html";
+	} else {
+            Nav.location.href  = manual + "-nav.html";
+            Main.location.href = manual +     ".html";
         }
     }
 }
@@ -33,6 +50,7 @@ function selectManual (manual)
 // Prepare the HTML for banner tabs
 function buildRow (manual)
 {
+    ///alert("window.name=" + window.name);
     var buf = "";
     var tag;
     for (var i = 0; i < manuals.length; i++) {
@@ -42,25 +60,28 @@ function buildRow (manual)
             tag = "TD";
         }
         buf += "<" + tag + ">";
-//        buf += "<A HREF=javascript:top.selectManual('" + manuals[i] + "'); >";
-        buf += "<A TARGET='_top' HREF='../manual/index.html?manual=" + manuals[i] + "' >";
-        buf += manuals[i];
+
+	if (manuals[i] == "api") {
+            buf += "<A TARGET='_top' HREF='../api/index.html' >";
+	} else if (manuals[i] == "notes") {
+            buf += "<A TARGET='_top' HREF='../wiki/notebook.html' >";
+        } else {
+            buf += "<A TARGET='_top' HREF='index.html?manual=" + manuals[i] + "' >";
+        }
+        buf += manualNames[i];
         buf += "</" + tag + ">";
     }
+    ///alert("buf=" + buf);
 
     return buf;
-}
-
-// Update the row of tabs
-function updateRow (manual)
-{
-    Banner.document.getElementById("bannerTabs").rows[0].innerHTML=buildRow(manual);
 }
 
 // Check whether the URL contains a manual name
 function checkSelection()
 {
+    
     var paramStr = "" + window.location.search;
+
     if (paramStr != "" && paramStr != "undefined") {
         paramStr = paramStr.substring(1); //Skip the '?' character
         var params = paramStr.split("&");
@@ -70,6 +91,8 @@ function checkSelection()
                 selectManual(pair[1]);
             }
         }
+    } else {
+       selectManual(""); // Use default
     }
 }
 
