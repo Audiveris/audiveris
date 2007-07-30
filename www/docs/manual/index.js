54dfdfe1601c28718f82258a9eb8908c7824bf9d
hbitteur <herve.bitteur@audiveris.org>
2007-07-30 21:29:29 +0000

    Improved versions

--- www/docs/manual/index.js
+++ www/docs/manual/index.js
@@ -109,3 +109,50 @@ function getCVSDate(s)
 
 // Dummy implementation when displaying javadoc files
 function loadFrames(){}
+
+// To include other HTML fragments
+// Copied from http://www.boutell.com/newfaq/creating/include.html
+function clientSideInclude(doc, id, url) {
+  var req = false;
+  // For Safari, Firefox, and other non-MS browsers
+  if (window.XMLHttpRequest) {
+    try {
+      req = new XMLHttpRequest();
+    } catch (e) {
+      req = false;
+    }
+  } else if (window.ActiveXObject) {
+    // For Internet Explorer on Windows
+    try {
+      req = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        req = false;
+      }
+    }
+  }
+ var element = doc.getElementById(id);
+ if (!element) {
+  alert("Bad id '" + id +
+   "' passed to clientSideInclude." +
+   "You need a div or span element " +
+   "with this id in your page.");
+  return;
+ }
+  if (req) {
+    // Synchronous request, wait till we have it all
+    req.open('GET', url, false);
+    req.send(null);
+    element.innerHTML = req.responseText;
+  } else {
+    element.innerHTML =
+   "Sorry, your browser does not support " +
+      "XMLHTTPRequest objects. This page requires " +
+      "Internet Explorer 5 or better for Windows, " +
+      "or Firefox for any system, or Safari. Other " +
+      "compatible browsers may also exist.";
+  }
+}
+
