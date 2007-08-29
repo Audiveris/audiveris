774761ec297895b8695ff9b1da5d4e00ec3540b1
hbitteur <herve.bitteur@audiveris.org>
2007-08-29 07:54:21 +0000

    Added javadoc items

--- src/main/omr/ui/util/SeparableMenu.java
+++ src/main/omr/ui/util/SeparableMenu.java
@@ -1,37 +1,65 @@
-/**
- * 
- */
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                         S e p a r a b l e M e n u                          //
+//                                                                            //
+//  Copyright (C) Brenton Partridge 2007. All rights reserved.                //
+//  This software is released under the GNU General Public License.           //
+//  Contact author at herve.bitteur@laposte.net to report bugs & suggestions. //
+//----------------------------------------------------------------------------//
+//
 package omr.ui.util;
 
 import javax.swing.*;
 
-public class SeparableMenu extends JMenu
+/**
+ * Class <code>SeparableMenu</code> is a menu which is able to collapse unneeded
+ * separators
+ *
+ * @author Brenton Partridge
+ * @version $Id$
+ */
+public class SeparableMenu
+    extends JMenu
 {
-	public SeparableMenu()
-	{
-		super();
-	}
+    //~ Constructors -----------------------------------------------------------
 
-	public SeparableMenu(Action action)
-	{
-		super(action);
-	}
+    public SeparableMenu ()
+    {
+        super();
+    }
 
-	public SeparableMenu(String s, boolean flag)
-	{
-		super(s, flag);
-	}
+    public SeparableMenu (Action action)
+    {
+        super(action);
+    }
 
-	public SeparableMenu(String s)
-	{
-		super(s);
-	}
+    public SeparableMenu (String  s,
+                          boolean flag)
+    {
+        super(s, flag);
+    }
 
-	@Override
-	public void addSeparator()
-	{
-		int count = super.getMenuComponentCount();
-		if (count > 0 && !(super.getMenuComponent(count - 1) instanceof JSeparator))
-			super.addSeparator();
-	}
+    public SeparableMenu (String s)
+    {
+        super(s);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //--------------//
+    // addSeparator //
+    //--------------//
+    /**
+     * The separator will be inserted only if it is really necessary
+     */
+    @Override
+    public void addSeparator ()
+    {
+        int count = super.getMenuComponentCount();
+
+        if ((count > 0) &&
+            !(super.getMenuComponent(count - 1) instanceof JSeparator)) {
+            super.addSeparator();
+        }
+    }
 }
