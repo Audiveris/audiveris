06fa7a65bf02cc6475a9275ad1f14d449485ed93
hbitteur <herve.bitteur@audiveris.org>
2007-11-20 21:08:37 +0000

    Score UI classes moved to omr.score.ui package

---
+++ src/main/omr/score/ui/ScoreActions.java
@@ -0,0 +1,320 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                          S c o r e A c t i o n s                           //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2007. All rights reserved.               //
+//  This software is released under the GNU General Public License.           //
+//  Contact author at herve.bitteur@laposte.net to report bugs & suggestions. //
+//----------------------------------------------------------------------------//
+//
+package omr.score.ui;
+
+import omr.Main;
+
+import omr.constant.Constant;
+import omr.constant.ConstantSet;
+import static omr.plugin.Dependency.*;
+import omr.plugin.Plugin;
+import static omr.plugin.PluginType.*;
+
+import omr.score.Score;
+import omr.score.entity.ScorePart;
+import omr.score.midi.MidiAgent;
+import omr.score.midi.MidiAgent.UnavailableException;
+
+import omr.sheet.SheetManager;
+
+import omr.step.Step;
+
+import omr.ui.ScoreBoard;
+import omr.ui.util.SwingWorker;
+
+import omr.util.Implement;
+import omr.util.Logger;
+
+import java.awt.event.*;
+import java.beans.*;
+
+import javax.swing.*;
+
+/**
+ * Class <code>ScoreActions</code> gathers actions related to scores
+ *
+ * @author Herv&eacute Bitteur
+ * @version $Id$
+ */
+public class ScoreActions
+{
+    //~ Static fields/initializers ---------------------------------------------
+
+    /** Specific application parameters */
+    private static final Constants constants = new Constants();
+
+    /** Usual logger utility */
+    private static final Logger logger = Logger.getLogger(ScoreActions.class);
+
+    //~ Methods ----------------------------------------------------------------
+
+    //-----------------//
+    // checkParameters //
+    //-----------------//
+    /**
+     * Make sure that the score parameters are properly set up, even by
+     * prompting the user for them, otherwise return false
+     *
+     * @param score the provided score
+     * @return true if OK, false otherwise
+     */
+    public static boolean checkParameters (Score score)
+    {
+        if (score.getTempo() == null) {
+            if (constants.promptParameters.getValue()) {
+                return defineParameters(score);
+            } else {
+                return setDefaultParameters(score);
+            }
+        } else {
+            return true;
+        }
+    }
+
+    //------------------//
+    // defineParameters //
+    //------------------//
+    /**
+     * Prompts the user for interactive confirmation or modification of
+     * score parameters
+     *
+     * @param score the provided score
+     * @return true if parameters are accepted, false if not
+     */
+    public static boolean defineParameters (Score score)
+    {
+        final boolean[]   result = new boolean[1];
+        final ScoreBoard  scoreBoard = new ScoreBoard("Parameters", score);
+        final JOptionPane optionPane = new JOptionPane(
+            scoreBoard.getComponent(),
+            JOptionPane.QUESTION_MESSAGE,
+            JOptionPane.OK_CANCEL_OPTION);
+
+        final JDialog     dialog = new JDialog(
+            Main.getGui().getFrame(),
+            score.getRadix() + " parameters",
+            true); // Modal flag
+        dialog.setContentPane(optionPane);
+
+        optionPane.addPropertyChangeListener(
+            new PropertyChangeListener() {
+                    public void propertyChange (PropertyChangeEvent e)
+                    {
+                        String prop = e.getPropertyName();
+
+                        if (dialog.isVisible() &&
+                            (e.getSource() == optionPane) &&
+                            (prop.equals(JOptionPane.VALUE_PROPERTY))) {
+                            Object obj = optionPane.getValue();
+                            int    value = ((Integer) obj).intValue();
+
+                            if (value == JOptionPane.OK_OPTION) {
+                                scoreBoard.commit();
+                                result[0] = true;
+                            } else {
+                                result[0] = false;
+                            }
+
+                            dialog.setVisible(false);
+                            dialog.dispose();
+                        }
+                    }
+                });
+        dialog.pack();
+        dialog.setVisible(true);
+
+        return result[0];
+    }
+
+    //----------------------//
+    // setDefaultParameters //
+    //----------------------//
+    private static boolean setDefaultParameters (Score score)
+    {
+        for (ScorePart scorePart : score.getPartList()) {
+            // Part name
+            scorePart.setName(scorePart.getDefaultName());
+
+            // Part midi program
+            scorePart.setMidiProgram(scorePart.getDefaultProgram());
+
+            // Replicate the score tempo
+            scorePart.setTempo(score.getDefaultTempo());
+        }
+
+        // Score global data
+        score.setTempo(score.getDefaultTempo());
+
+        return true;
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    //--------------//
+    // BrowseAction //
+    //--------------//
+    /**
+     * Class <code>BrowseAction</code> launches the tree display of the current
+     * score.
+     */
+    @Plugin(type = SCORE_EDIT, dependency = SCORE_AVAILABLE, onToolbar = true)
+    public static class BrowseAction
+        extends AbstractAction
+    {
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public void actionPerformed (ActionEvent e)
+        {
+            ScoreController.getCurrentScore()
+                           .viewScore();
+        }
+    }
+
+    //------------//
+    // DumpAction //
+    //------------//
+    /**
+     * Class <code>DumpAction</code> dumps the internals of a score to system
+     * output
+     *
+     */
+    @Plugin(type = SCORE_EDIT, dependency = SCORE_AVAILABLE, onToolbar = true)
+    public static class DumpAction
+        extends AbstractAction
+    {
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public void actionPerformed (ActionEvent e)
+        {
+            ScoreController.getCurrentScore()
+                           .dump();
+        }
+    }
+
+    //-------------//
+    // ParamAction //
+    //-------------//
+    /**
+     * Class <code>ParamAction</code> launches the dialog to set up score
+     * parameters.
+     */
+    @Plugin(type = SCORE_EDIT, dependency = SCORE_AVAILABLE, onToolbar = true)
+    public static class ParamAction
+        extends AbstractAction
+    {
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public void actionPerformed (ActionEvent e)
+        {
+            Score score = ScoreController.getCurrentScore();
+
+            if (defineParameters(score)) {
+                // Invalidate the midi sequence
+                try {
+                    MidiAgent agent = MidiAgent.getInstance();
+
+                    if (agent.getScore() == score) {
+                        agent.reset();
+                    }
+                } catch (UnavailableException ex) {
+                    logger.warning("Cannot reset Midi sequence", ex);
+                }
+            }
+        }
+    }
+
+    //---------------//
+    // RebuildAction //
+    //---------------//
+    /**
+     * Class <code>RebuildAction</code> re-translates all sheet glyphs to score
+     * entities.
+     */
+    @Plugin(type = SCORE_EDIT, dependency = SCORE_AVAILABLE, onToolbar = true)
+    public static class RebuildAction
+        extends AbstractAction
+    {
+        //~ Methods ------------------------------------------------------------
+
+        @Implement(ActionListener.class)
+        public void actionPerformed (ActionEvent e)
+        {
+            final SwingWorker worker = new SwingWorker() {
+                public Object construct ()
+                {
+                    try {
+                        SheetManager.getSelectedSheet()
+                                    .getSheetSteps()
+                                    .doit(Step.SCORE);
+                    } catch (Exception ex) {
+                        logger.warning("Could not refresh score", ex);
+                    }
+
+                    return null;
+                }
+            };
+
+            worker.start();
+        }
+    }
+
+    //-------------//
+    // StoreAction //
+    //-------------//
+    /**
+     * Class <code>StoreAction</code> handles the saving of the currently
+     * selected score, using MusicXML format.
+     */
+    @Plugin(type = SCORE_EXPORT, dependency = SCORE_AVAILABLE, onToolbar = true)
+    public static class StoreAction
+        extends AbstractAction
+    {
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public void actionPerformed (ActionEvent e)
+        {
+            final SwingWorker worker = new SwingWorker() {
+                public Object construct ()
+                {
+                    Score score = ScoreController.getCurrentScore();
+
+                    if (checkParameters(score)) {
+                        try {
+                            score.export();
+                        } catch (Exception ex) {
+                            logger.warning("Could not store " + score, ex);
+                        }
+                    }
+
+                    return null;
+                }
+            };
+
+            worker.start();
+        }
+    }
+
+    //-----------//
+    // Constants //
+    //-----------//
+    private static final class Constants
+        extends ConstantSet
+    {
+        //~ Instance fields ----------------------------------------------------
+
+        Constant.Boolean promptParameters = new Constant.Boolean(
+            true,
+            "Should we prompt the user for score parameters?");
+    }
+}
---
+++ src/main/omr/score/ui/ScoreView.java
@@ -0,0 +1,435 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                             S c o r e V i e w                              //
+//                                                                            //
+//  Copyright (C) Herve Bitteur 2000-2007. All rights reserved.               //
+//  This software is released under the GNU General Public License.           //
+//  Contact author at herve.bitteur@laposte.net to report bugs & suggestions. //
+//----------------------------------------------------------------------------//
+//
+package omr.score.ui;
+
+import omr.constant.ConstantSet;
+
+import omr.score.Score;
+import omr.score.ScoreSheetBridge;
+import omr.score.common.PagePoint;
+import omr.score.common.ScorePoint;
+import omr.score.common.SystemPoint;
+import omr.score.common.UnitDimension;
+import omr.score.entity.Measure;
+import omr.score.entity.Slot;
+import omr.score.entity.System;
+import static omr.score.ui.ScoreConstants.*;
+import omr.score.visitor.ScorePainter;
+
+import omr.selection.Selection;
+import omr.selection.SelectionHint;
+import omr.selection.SelectionTag;
+
+import omr.sheet.Sheet;
+
+import omr.ui.PixelCount;
+import omr.ui.SheetAssembly;
+import omr.ui.util.Panel;
+import omr.ui.view.Rubber;
+import omr.ui.view.RubberZoomedPanel;
+import omr.ui.view.ScrollView;
+import omr.ui.view.Zoom;
+import omr.ui.view.ZoomedPanel;
+
+import omr.util.Logger;
+
+import java.awt.*;
+import java.util.*;
+
+import javax.swing.*;
+
+/**
+ * Class <code>ScoreView</code> encapsulates the horizontal display of all the
+ * score systems which may encompass several pages & systems. It knows about the
+ * display zoom ratio of the score.
+ *
+ * <dl>
+ * <dt><b>Selection Inputs:</b></dt><ul>
+ * <li>SCORE Location
+ * </ul>
+ * </dl>
+ *
+ * @author Herv&eacute; Bitteur
+ * @version $Id$
+ */
+public class ScoreView
+{
+    //~ Static fields/initializers ---------------------------------------------
+
+    /** Specific application parameters */
+    private static final Constants constants = new Constants();
+
+    /** Usual logger utility */
+    private static final Logger logger = Logger.getLogger(ScoreView.class);
+
+    //~ Instance fields --------------------------------------------------------
+
+    /** The info zone */
+    private final JLabel info = new JLabel("Score");
+
+    /** The displayed panel */
+    private final MyPanel panel;
+
+    /** The scroll pane + info zone */
+    private final Panel compound = new Panel();
+
+    /** The related score */
+    private final Score score;
+
+    /** The scroll pane */
+    private final ScrollView pane;
+
+    /** Display zoom */
+    private final Zoom zoom = new Zoom(0.5d);
+
+    /** Mouse rubber */
+    private final Rubber rubber = new Rubber(zoom);
+
+    //~ Constructors -----------------------------------------------------------
+
+    //-----------//
+    // ScoreView //
+    //-----------//
+    /**
+     * Create the view dedicated to the display of the provided score.
+     *
+     * @param score the score to display
+     */
+    public ScoreView (Score score)
+    {
+        if (logger.isFineEnabled()) {
+            logger.fine("new ScoreView on " + score);
+        }
+
+        panel = new MyPanel(zoom, rubber);
+        pane = new ScrollView(panel);
+
+        // Explicitly register the display to Score Selection
+        Sheet     sheet = score.getSheet();
+        Selection locationSelection = sheet.getSelection(
+            SelectionTag.SCORE_RECTANGLE);
+        panel.setLocationSelection(locationSelection);
+        locationSelection.addObserver(panel);
+
+        // Layout
+        info.setHorizontalAlignment(SwingConstants.LEFT);
+        compound.setLayout(new BorderLayout());
+        compound.add(pane.getComponent(), BorderLayout.CENTER);
+        compound.add(info, BorderLayout.SOUTH);
+
+        // Cross referencing between score and its view
+        this.score = score;
+        score.setView(this);
+
+        // Compute origin of all members
+        computeModelSize();
+
+        // Bridge companion between Score and Sheet locations both ways
+        new ScoreSheetBridge(score);
+
+        // Force selection update
+        Selection pixelSelection = sheet.getSelection(
+            SelectionTag.SHEET_RECTANGLE);
+        pixelSelection.reNotifyObservers(null);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    //--------------//
+    // getComponent //
+    //--------------//
+    /**
+     * Report the encapsulated UI component
+     *
+     * @return the UI component
+     */
+    public Component getComponent ()
+    {
+        return compound;
+    }
+
+    //----------//
+    // getScore //
+    //----------//
+    /**
+     * Report the score this view is dedicated to
+     *
+     * @return the related score
+     */
+    public Score getScore ()
+    {
+        return score;
+    }
+
+    //---------------//
+    // getScrollPane //
+    //---------------//
+    /**
+     * Report the underlying ScrollView
+     *
+     * @return the Scroll Pane
+     */
+    public ScrollView getScrollPane ()
+    {
+        return pane;
+    }
+
+    //-------//
+    // close //
+    //-------//
+    /**
+     * Close the score display, by removing it from the enclosing tabbed pane.
+     */
+    public void close ()
+    {
+        if (logger.isFineEnabled()) {
+            logger.fine("Closing scoreView");
+        }
+
+        Sheet sheet = score.getSheet();
+
+        if (sheet != null) {
+            SheetAssembly assembly = sheet.getAssembly();
+
+            if (assembly != null) {
+                assembly.closeScoreView();
+            }
+        }
+    }
+
+    //------------------//
+    // computeModelSize //
+    //------------------//
+    /**
+     * Run computations on the tree of score, systems, etc, so that all display
+     * data, such as origins and widths are available for display use.
+     */
+    public void computeModelSize ()
+    {
+        // Should be a visitor !!! TBD
+
+        // Determine the X value of the whole set of systems
+        int totalWidth = 0;
+
+        for (Iterator it = score.getChildren()
+                                .iterator(); it.hasNext();) {
+            System system = (System) it.next();
+            totalWidth += (system.getDimension().width + INTER_SYSTEM);
+        }
+
+        // Total size of this panel (for proper scrolling)
+        int h = (2 * STAFF_MARGIN_HEIGHT) +
+                (score.getMaxStaffNumber() * STAFF_AREA_HEIGHT);
+        panel.setModelSize(
+            new Dimension(SCORE_INIT_X + totalWidth + INTER_SYSTEM, 2 * h));
+    }
+
+    //---------//
+    // repaint //
+    //---------//
+    public void repaint ()
+    {
+        pane.getComponent()
+            .repaint();
+    }
+
+    //----------//
+    // toString //
+    //----------//
+    /**
+     * Report a readable description
+     *
+     * @return a string based on the score XML file name
+     */
+    @Override
+    public String toString ()
+    {
+        return "{ScoreView " + score.getRadix() + "}";
+    }
+
+    //-----------//
+    // tellPoint //
+    //-----------//
+    private void tellPoint (ScorePoint  scrPt,
+                            PagePoint   pagPt,
+                            SystemPoint sysPt)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        //        if (scrPt != null) {
+        //            sb.append("ScorePoint")
+        //              .append(" X:")
+        //              .append(scrPt.x)
+        //              .append(" Y:")
+        //              .append(scrPt.y)
+        //              .append(" ");
+        //        }
+        if (pagPt != null) {
+            sb.append("PagePoint")
+              .append(" X:")
+              .append(pagPt.x)
+              .append(" Y:")
+              .append(pagPt.y)
+              .append(" ");
+        }
+
+        if (sysPt != null) {
+            sb.append("SystemPoint")
+              .append(" X:")
+              .append(sysPt.x)
+              .append(" Y:")
+              .append(sysPt.y)
+              .append(" ");
+        }
+
+        info.setText(sb.toString());
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    //-------//
+    // Panel //
+    //-------//
+    public class MyPanel
+        extends RubberZoomedPanel
+    {
+        //~ Instance fields ----------------------------------------------------
+
+        /** Currently highlighted slot */
+        private Slot slot;
+        private Measure measure;
+
+        //~ Constructors -------------------------------------------------------
+
+        //---------//
+        // MyPanel //
+        //---------//
+        MyPanel (Zoom   zoom,
+                 Rubber rubber)
+        {
+            super(zoom, rubber);
+            setName("ScoreView-MyPanel");
+            rubber.setMouseMonitor(this);
+
+            // Initialize drawing colors, border, opacity.
+            setBackground(new Color(255, 255, 220));
+            setForeground(Color.black);
+        }
+
+        //~ Methods ------------------------------------------------------------
+
+        //-----------//
+        // highLight //
+        //-----------//
+        public void highLight (Measure measure,
+                               Slot    slot)
+        {
+            this.measure = measure;
+            this.slot = slot;
+
+            int margin = constants.measureMargin.getValue();
+
+            // Safer
+            if ((measure == null) || (slot == null)) {
+                repaint(); // To erase previous highlight
+
+                return;
+            }
+
+            UnitDimension dimension = measure.getSystem()
+                                             .getDimension();
+            Point         origin = measure.getDisplayOrigin();
+            ZoomedPanel   zp = (ZoomedPanel) this;
+
+            // If the current measure is at the beginning of a system, 
+            // make the most of this (new) system as visible as possible
+            if (measure.getPreviousSibling() == null) {
+                Rectangle rect = new Rectangle(
+                    origin.x,
+                    origin.y,
+                    dimension.width,
+                    dimension.height + STAFF_HEIGHT);
+                zp.showFocusLocation(rect);
+            }
+
+            // Make the measure rectangle visible
+            Rectangle rect = new Rectangle(
+                (origin.x + measure.getLeftX()) - margin,
+                origin.y - margin,
+                measure.getWidth() + (2 * margin),
+                dimension.height + STAFF_HEIGHT + (2 * margin));
+            zp.showFocusLocation(rect);
+        }
+
+        //--------//
+        // render //
+        //--------//
+        @Override
+        public void render (Graphics g)
+        {
+            ScorePainter painter = new ScorePainter(g, zoom);
+            score.accept(painter);
+
+            if (slot != null) {
+                painter.drawSlot(measure, slot, Color.MAGENTA);
+            }
+        }
+
+        //--------//
+        // update //
+        //--------//
+        /**
+         * Call-back (part of Observer interface) triggered when location has
+         * been modified
+         *
+         * @param selection the notified Selection
+         * @param hint potential notification hint
+         */
+        @Override
+        public void update (Selection     selection,
+                            SelectionHint hint)
+        {
+            if (logger.isFineEnabled()) {
+                logger.fine("ScoreView: Score selection updated " + selection);
+            }
+
+            // Show location in display (default dehavior)
+            super.update(selection, hint);
+
+            // Display location coordinates
+            if (selection == locationSelection) {
+                Rectangle rect = (Rectangle) selection.getEntity();
+
+                if (rect != null) {
+                    ScorePoint  scrPt = new ScorePoint(rect.x, rect.y);
+                    System      system = score.scoreLocateSystem(scrPt);
+                    PagePoint   pagPt = system.toPagePoint(scrPt);
+                    SystemPoint sysPt = system.toSystemPoint(pagPt);
+                    tellPoint(scrPt, pagPt, sysPt);
+                } else {
+                    tellPoint(null, null, null);
+                }
+            }
+        }
+    }
+
+    //-----------//
+    // Constants //
+    //-----------//
+    private static final class Constants
+        extends ConstantSet
+    {
+        //~ Instance fields ----------------------------------------------------
+
+        PixelCount measureMargin = new PixelCount(
+            10,
+            "Number of pixels as margin when highlighting a measure");
+    }
+}
