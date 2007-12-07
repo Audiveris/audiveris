07104e7d51fc3a61646946b5f35777d39fc0edfb
bpartridge <bapartridge@gmail.com>
2007-12-07 11:27:19 +0000

    Updated documentation

--- src/main/omr/ui/MacApplication.java
+++ src/main/omr/ui/MacApplication.java
@@ -1,106 +1,119 @@
 //----------------------------------------------------------------------------//
 //                                                                            //
-//                               MacApplication                               //
+//                         M a c A p p l i c a t i o n                        //
 //                                                                            //
 //  Copyright (C) Brenton Partridge 2007. All rights reserved.                //
 //  This software is released under the GNU General Public License.           //
 //  Contact herve.bitteur@laposte.net to report bugs & suggestions.           //
 //----------------------------------------------------------------------------//
 //
-
 package omr.ui;
-
-import java.lang.reflect.*;
-
-import javax.swing.Action;
 
 import omr.util.Implement;
 import omr.util.Logger;
 
+import java.awt.event.ActionListener;
+import java.lang.reflect.*;
+
 /**
- * Class <code>MacApplication</code> provides dynamic hooks into the 
- * OSX-only eawt package, registering Audiveris actions for the 
+ * Class <code>MacApplication</code> provides dynamic hooks into the
+ * OSX-only eawt package, registering Audiveris actions for the
  * Preferences, About, and Quit menu items.
- * 
+ *
  * @author Brenton Partridge
  * @version $Id$
  */
-public class MacApplication implements InvocationHandler
+public class MacApplication
+    implements InvocationHandler
 {
-    private final Action options;
-    private final Action exit;
-    private final Action about;
-    
-    private MacApplication (Action options, Action exit, Action about)
-    {
-        this.options = options;
-        this.exit = exit;
-        this.about = about;
-    }
-    
+    //~ Static fields/initializers ---------------------------------------------
+
+    /** Usual logger utility */
+    private static final Logger logger = Logger.getLogger(MacApplication.class);
+
+    //~ Instance fields --------------------------------------------------------
+
+    private final ActionListener options = new GuiActions.OptionsAction();
+    private final ActionListener exit = new GuiActions.ExitAction();
+    private final ActionListener about = new GuiActions.AboutAction();
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * Invocation handler for <code>
+     * com.apple.eawt.ApplicationListener</code>.
+     * This method should not be manually called;
+     * it is used by the proxy to forward calls.
+     * @throws Throwable
+     */
     @Implement(InvocationHandler.class)
-    public Object invoke (Object proxy, Method method, Object[] args)
-    throws Throwable
+    public Object invoke (Object   proxy,
+                          Method   method,
+                          Object[] args)
+        throws Throwable
     {
         String name = method.getName();
-        if ("handlePreferences".equals(name))
-        {
+
+        if ("handlePreferences".equals(name)) {
             logger.fine(name);
             options.actionPerformed(null);
-        }
-        else if ("handleQuit".equals(name))
-        {
+        } else if ("handleQuit".equals(name)) {
             logger.fine(name);
             exit.actionPerformed(null);
-        }
-        else if ("handleAbout".equals(name))
-        {
+        } else if ("handleAbout".equals(name)) {
             logger.fine(name);
             about.actionPerformed(null);
         }
+
         return null;
     }
-    
-    /** Usual logger utility */
-    private static final Logger logger = Logger.getLogger(MacApplication.class);
-    
+
     /**
      * Registers actions for preferences, about, and quit.
      * @return true if successful, false if platform is not
      * Mac OS X or if an error occurs
      */
-    public static boolean setupMacMenus()
+    public static boolean setupMacMenus ()
     {
-        if (!omr.Main.MAC_OS_X) return false;
-        try
-        {
-            Class appClass = Class.forName("com.apple.eawt.Application");
-            Class listenerClass = Class.forName("com.apple.eawt.ApplicationListener");
+        if (!omr.Main.MAC_OS_X) {
+            return false;
+        }
+
+        try {
+            //The class used to register hooks
+            Class  appClass = Class.forName("com.apple.eawt.Application");
             Object app = appClass.newInstance();
-            
-            //Enable menus
-            for (String methodName : 
-                new String[] {"setEnabledAboutMenu", "setEnabledPreferencesMenu"})
-            {
+
+            //Enable the about menu item and the preferences menu item
+            for (String methodName : new String[] {
+                     "setEnabledAboutMenu", "setEnabledPreferencesMenu"
+                 }) {
                 Method method = appClass.getMethod(methodName, boolean.class);
                 method.invoke(app, true);
             }
-            
-            //Add an anonymous listener
-            Action about = new GuiActions.AboutAction(),
-            options = new GuiActions.OptionsAction(),
-            exit = new GuiActions.ExitAction();
+
+            //The interface used to register hooks
+            Class  listenerClass = Class.forName(
+                "com.apple.eawt.ApplicationListener");
+
+            //Using the current class loader,
+            //generate, load, and instantiate a class implementing listenerClass,
+            //providing an instance of this class as a callback for any method invocation
             Object listenerProxy = Proxy.newProxyInstance(
-                MacApplication.class.getClassLoader(), 
-                new Class[] {listenerClass},
-                new MacApplication(options, exit, about));
-            Method addListener = appClass.getMethod("addApplicationListener", listenerClass);
+                MacApplication.class.getClassLoader(),
+                new Class[] { listenerClass },
+                new MacApplication());
+
+            //Add the generated class as a hook
+            Method addListener = appClass.getMethod(
+                "addApplicationListener",
+                listenerClass);
             addListener.invoke(app, listenerProxy);
+
             return true;
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             logger.warning("Unable to setup Mac OS X GUI integration", ex);
+
             return false;
         }
     }
