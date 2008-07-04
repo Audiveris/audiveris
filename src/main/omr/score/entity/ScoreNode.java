c5ac0a754e9dc54db1d22bd3b5e2c8bc3c90906f
hbitteur <herve.bitteur@audiveris.org>
2008-07-04 20:38:35 +0000

    now extends VisitableNode

--- src/main/omr/score/entity/ScoreNode.java
+++ src/main/omr/score/entity/ScoreNode.java
@@ -10,12 +10,9 @@
 package omr.score.entity;
 
 import omr.score.Score;
-import omr.score.visitor.ScoreVisitor;
-import omr.score.visitor.Visitable;
 
 import omr.sheet.Scale;
 
-import omr.util.Implement;
 import omr.util.Logger;
 import omr.util.TreeNode;
 
@@ -23,59 +20,11 @@ import omr.util.TreeNode;
  * Class <code>ScoreNode</code> handles a node in the tree hierarchy of a score
  * entity.
  *
- * <p>
- * The use of TreeNode for the Score hierarchy is the following :
- * <pre>
- *                               Score                                                      |
- *                                 O                                                        |
- *                                 |                                                        |
- *                                 |                                                        |
- *                               Page  (not yet implemented)                                |
- *                                 O                                                        |
- *                                 |                                                        |
- *                                 |                                                        |
- *                              System                                                      |
- *                                 O                                                        |
- *                                 |                                                        |
- *                                 |                                                        |
- *                            SystemPart                                                    |
- *                                 O                                                        |
- *                                 |                                                        |
- *          _________________________________________________                               |
- *         {startingBarline, StaffList, MeasureList, SlurList}                              |
- *                                O            O         O                                  |
- *                                |            |         |                                  |
- *                                |            |         |                                  |
- *                              Staff       Measure    Slur                                 |
- *                                             O                                            |
- *                                             |                                            |
- * _______________________________________________________________________________________  |
- *{ClefList, KeyList, Time, ChordList, BeamList, LyricList, TextList, DynamicList, Barline} |
- *     O        O       O        O         O          O         O            O              |
- *     |        |       |        |         |          |         |            |              |
- *     |        |       |        |         |          |         |            |              |
- *   Clef    KeySig. TimeSig.  Chord     Beam       Lyric     Text        Dynamic           |
- *                               O                    O                                     |
- *                               |                    |                                     |
- *                               |                    |                                     |
- *                             Note               Syllable                                  |
- *  </pre>
- * </p>
- *
- * <p> Since the various score entities are organized in a tree of ScoreNode
- * instances, we use a lot the ability to browse the complete hierarchy,
- * starting from the top (the score instance).</p>
- *
- * <p> This is implemented via score visitors, with the added functionality that
- * any specific visited node can stop the visit to nodes below it in the
- * hierarchy.
- *
  * @author Herv&eacute; Bitteur
  * @version $Id$
  */
 public class ScoreNode
-    extends TreeNode
-    implements Visitable
+    extends VisitableNode
 {
     //~ Static fields/initializers ---------------------------------------------
 
@@ -97,7 +46,7 @@ public class ScoreNode
      *
      * @param container the containing node, or null otherwise
      */
-    public ScoreNode (ScoreNode container)
+    public ScoreNode (VisitableNode container)
     {
         super(container);
 
@@ -112,35 +61,6 @@ public class ScoreNode
     }
 
     //~ Methods ----------------------------------------------------------------
-
-    //--------//
-    // accept //
-    //--------//
-    @Implement(Visitable.class)
-    public boolean accept (ScoreVisitor visitor)
-    {
-        return visitor.visit(this);
-    }
-
-    //----------------//
-    // acceptChildren //
-    //----------------//
-    /**
-     * Pattern to traverse the children of this node
-     *
-     * @param visitor concrete visitor object to define the actual processing
-     */
-    public void acceptChildren (ScoreVisitor visitor)
-    {
-        ///logger.info(children.size() + " children for " + this);
-        for (TreeNode node : getChildrenCopy()) {
-            ScoreNode child = (ScoreNode) node;
-
-            if (child.accept(visitor)) {
-                child.acceptChildren(visitor);
-            }
-        }
-    }
 
     //------------------//
     // getContextString //
