52175ff67270a212f10c6f17d8e25c16b32e18f9
hbitteur <herve.bitteur@audiveris.org>
2008-09-19 17:35:53 +0000

    Formatting

--- src/main/omr/score/entity/Arpeggiate.java
+++ src/main/omr/score/entity/Arpeggiate.java
@@ -13,11 +13,11 @@ import omr.constant.ConstantSet;
 
 import omr.glyph.Glyph;
 
+import omr.score.common.PixelPoint;
+import omr.score.common.PixelRectangle;
 import omr.score.common.SystemPoint;
 import omr.score.visitor.ScoreVisitor;
 
-import omr.score.common.PixelPoint;
-import omr.score.common.PixelRectangle;
 import omr.sheet.Scale;
 
 import omr.util.Logger;
