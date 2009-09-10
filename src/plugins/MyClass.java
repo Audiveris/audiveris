7aa6d6447d5382323cfa61db5339b171eaa91b97
hbitteur <herve.bitteur@audiveris.org>
2009-09-10 19:04:59 +0000

    Autofold the header lines

--- src/plugins/MyClass.java
+++ src/plugins/MyClass.java
@@ -2,11 +2,13 @@
 //                                                                            //
 //                               M y C l a s s                                //
 //                                                                            //
+//----------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">                          //
 //  Copyright (C) Herve Bitteur 2000-2009. All rights reserved.               //
 //  This software is released under the GNU General Public License.           //
 //  Please contact users@audiveris.dev.java.net to report bugs & suggestions. //
 //----------------------------------------------------------------------------//
-//
+// </editor-fold>
 import omr.score.Score;
 import omr.score.ui.ScoreController;
 import omr.score.ui.ScoreDependent;
