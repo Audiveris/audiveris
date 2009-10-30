7aecef111a6300fb5d24813fcde864a116bca38b
hbitteur <herve.bitteur@audiveris.org>
2009-10-30 22:37:44 +0000

    DotTranslator class renamed as DotTranslation

--- src/main/omr/score/SystemTranslator.java
+++ src/main/omr/score/SystemTranslator.java
@@ -29,7 +29,7 @@ import omr.score.entity.BeamItem;
 import omr.score.entity.Chord;
 import omr.score.entity.Clef;
 import omr.score.entity.Coda;
-import omr.score.entity.DotTranslator;
+import omr.score.entity.DotTranslation;
 import omr.score.entity.Fermata;
 import omr.score.entity.KeySignature;
 import omr.score.entity.Measure;
@@ -180,7 +180,7 @@ public class SystemTranslator
         // Flag (-> chord)
         translate(new FlagTranslator());
         // Augmentation dots (-> chord)
-        translate(new MyDotTranslator());
+        translate(new DotTranslator());
         // Tuplets
         translate(new TupletTranslator());
         // Finalize measure ties, voices & durations
@@ -571,6 +571,32 @@ public class SystemTranslator
         }
     }
 
+    //---------------//
+    // DotTranslator //
+    //---------------//
+    private class DotTranslator
+        extends Translator
+    {
+        //~ Constructors -------------------------------------------------------
+
+        public DotTranslator ()
+        {
+            super("Dot");
+        }
+
+        //~ Methods ------------------------------------------------------------
+
+        public boolean isRelevant (Glyph glyph)
+        {
+            return glyph.getShape() == Shape.DOT;
+        }
+
+        public void translate (Glyph glyph)
+        {
+            DotTranslation.populateDot(glyph, currentMeasure, currentCenter);
+        }
+    }
+
     //--------------------//
     // DynamicsTranslator //
     //--------------------//
@@ -778,32 +804,6 @@ public class SystemTranslator
 
         public void translate (Glyph glyph)
         {
-        }
-    }
-
-    //-----------------//
-    // MyDotTranslator //
-    //-----------------//
-    private class MyDotTranslator
-        extends Translator
-    {
-        //~ Constructors -------------------------------------------------------
-
-        public MyDotTranslator ()
-        {
-            super("Dot");
-        }
-
-        //~ Methods ------------------------------------------------------------
-
-        public boolean isRelevant (Glyph glyph)
-        {
-            return glyph.getShape() == Shape.DOT;
-        }
-
-        public void translate (Glyph glyph)
-        {
-            DotTranslator.populateDot(glyph, currentMeasure, currentCenter);
         }
     }
 
