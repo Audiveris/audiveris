943c8364650a11cebbafe07283d2bb74f9662881
hbitteur <herve.bitteur@audiveris.org>
2011-10-10 10:24:42 +0200

    Fomer Lag and Vertex classes abstracted to interfaces

---
+++ src/main/omr/graph/Vertex.java
@@ -0,0 +1,131 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                                V e r t e x                                 //
+//                                                                            //
+//----------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">                          //
+//  Copyright (C) Herve Bitteur 2000-2010. All rights reserved.               //
+//  This software is released under the GNU General Public License.           //
+//  Goto http://kenai.com/projects/audiveris to report bugs or suggestions.   //
+//----------------------------------------------------------------------------//
+// </editor-fold>
+package omr.graph;
+
+import java.util.List;
+
+/**
+ * Interface {@code Vertex} encapsulates a Vertex (or Node) in a directed
+ * graph. Any vertex can have incoming edges from other vertices and outgoing
+ * edges to other vertices.
+ *
+ * <p>The Vertex can have a list of related {@code VertexView}'s.
+ * All the vertices in the graph have parallel lists of {@code VertexView}'s as
+ * the Digraph itself which has a parallel list of {@code DigraphView}'s.
+ *
+ * @param <D> type for enclosing digraph precise subtype
+ * @param <V> type for Vertex precise subtype
+ *
+ * @author Hervé Bitteur
+ */
+public interface Vertex<D extends Digraph, V extends Vertex<D, V>>
+{
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * Assign the containing graph of this vertex
+     * @param graph The hosting graph
+     */
+    public void setGraph (D graph);
+
+    /**
+     * Report the containing graph of this vertex
+     * @return the containing graph
+     */
+    public D getGraph ();
+
+    /**
+     * Assign a new Id (for expert use only)
+     * @param id The assigned id
+     */
+    public void setId (int id);
+
+    /**
+     * Report the unique Id (within the containing graph) of this vertex
+     * @return the id
+     */
+    public int getId ();
+
+    /**
+     * Return how many incoming edges we have
+     * @return the number of incomings
+     */
+    public int getInDegree ();
+
+    /**
+     * Return the number of edges outgoing from this vertex
+     * @return the number of outgoings
+     */
+    public int getOutDegree ();
+
+    /**
+     * An access to incoming vertices
+     * @return the incoming vertices
+     */
+    public List<V> getSources ();
+
+    /**
+     * Return an access to the outgoing vertices of this vertex
+     * @return the outgoing vertices
+     */
+    public List<V> getTargets ();
+
+    /**
+     * Report the view at given index
+     * @param index index of the desired view
+     * @return the desired view
+     */
+    public VertexView getView (int index);
+
+    /**
+     * Report the current number of views on this Vertex
+     * @return the current number of views
+     */
+    public int getViewsCount ();
+
+    /**
+     * Create an edge between this vertex and the target vertax
+     * @param target arrival vertex
+     */
+    public void addTarget (V target);
+
+    /**
+     * Add a related view of this vertex
+     * @param view the view to be linked
+     */
+    public void addView (VertexView view);
+
+    /**
+     * Get rid of all views for this vertex
+     */
+    public void clearViews ();
+
+    /**
+     * Delete this vertex. This implies also the removal of all its incoming and
+     * outgoing edges.
+     */
+    public void delete ();
+
+    /**
+     * Prints on standard output a detailed information about this vertex.
+     */
+    public void dump ();
+
+    /**
+     * Remove an edge between this vertex and a target vertex
+     *
+     * @param target arrival vertex
+     * @param strict throw RuntimeException if the edge does not exist
+     */
+    public void removeTarget (V       target,
+                              boolean strict);
+}
---
+++ src/main/omr/lag/Lag.java
@@ -0,0 +1,199 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                                   L a g                                    //
+//                                                                            //
+//----------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">                          //
+//  Copyright (C) Herve Bitteur 2000-2010. All rights reserved.               //
+//  This software is released under the GNU General Public License.           //
+//  Goto http://kenai.com/projects/audiveris to report bugs or suggestions.   //
+//----------------------------------------------------------------------------//
+// </editor-fold>
+package omr.lag;
+
+import omr.graph.Digraph;
+
+import omr.run.Oriented;
+import omr.run.Run;
+import omr.run.RunsTable;
+
+import omr.score.common.PixelPoint;
+import omr.score.common.PixelRectangle;
+
+import omr.selection.SectionEvent;
+import omr.selection.SectionIdEvent;
+import omr.selection.SectionSetEvent;
+import omr.selection.SelectionService;
+
+import omr.util.Predicate;
+
+import java.awt.Rectangle;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Interface <code>Lag</code> defines a graph of {@link Section} instances
+ * (sets of contiguous runs with compatible lengths), linked by Junctions when
+ * there is no more contiguous run or when the compatibility is no longer met.
+ *
+ * Sections are thus vertices of the graph, while junctions are directed edges
+ * between sections. All the sections (and runs) have the same orientation
+ * shared by the lag.
+ *
+ * <p>A lag may have a related UI selection service accessible through {@link
+ * #getSectionService}. This selection service handles Section, SectionId and
+ * SectionSet events. The {@link #getSelectedSection} and
+ * {@link #getSelectedSectionSet} methods are just convenient ways to retrieve
+ * the last selected section, sectionId or sectionSet from the lag selection
+ * service.</p>
+ *
+ * <p>Run selection is provided by a separate selection service hosted by the
+ * underlying RunsTable instance. For convenience, one can use the method
+ * {@link #getRunService()} to get access to this run service.</p>
+ *
+ * @author Hervé Bitteur
+ */
+public interface Lag
+    extends Digraph<Lag, Section>, Oriented
+{
+    //~ Static fields/initializers ---------------------------------------------
+
+    /** Events that can be published on lag section service */
+    static final Class[] eventsWritten = new Class[] {
+                                             SectionIdEvent.class,
+                                             SectionEvent.class,
+                                             SectionSetEvent.class
+                                         };
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * Return the first run (the one with minimum position, then with minimum
+     * coordinate) found in the given rectangle
+     *
+     * @param coordMin min abscissa for horizontal lag
+     * @param coordMax max abscissa for horizontal lag
+     * @param posMin   min ordinate for horizontal lag
+     * @param posMax   max ordinate for horizontal lag
+     *
+     * @return the run, or null if none found
+     */
+    Run getFirstRectRun (int coordMin,
+                         int coordMax,
+                         int posMin,
+                         int posMax);
+
+    /**
+     * Report the run found at given coordinates, if any.
+     *
+     * @param x absolute abscissa
+     * @param y absolute ordinate
+     * @return  the run found, or null otherwise
+     */
+    Run getRunAt (int x,
+                  int y);
+
+    /**
+     * Report the selection service for runs
+     * @return the run selection service
+     */
+    SelectionService getRunService ();
+
+    /**
+     * Use the provided runs table as the lag underlying table
+     * @param runsTable the populated runs
+     */
+    void setRuns (RunsTable runsTable);
+
+    /**
+     * Report the section selection service
+     * @return the section selection service
+     */
+    SelectionService getSectionService ();
+
+    /**
+     * Return a view of the collection of sections that are currently part of
+     * this lag
+     *
+     * @return the sections collection
+     */
+    Collection<Section> getSections ();
+
+    /**
+     * Return the collection of sections which intersect the provided rectangle
+     *
+     * @param rect the rectangular area to be checked, specified in the usual
+     * (coord, pos) form.
+     *
+     * @return the list of sections found (may be empty)
+     */
+    List<Section> getSectionsIn (Rectangle rect);
+
+    /**
+     * Convenient method to report the UI currently selected Section, if any, in
+     * this lag
+     * @return the UI selected section, or null if none
+     */
+    Section getSelectedSection ();
+
+    /**
+     * Convenient method to report the UI currently selected set of Sections,
+     * if any, in this lag
+     * @return the UI selected section set, or null if none
+     */
+    Set<Section> getSelectedSectionSet ();
+
+    /**
+     * Inject dependency about other services for lag
+     * @param locationService the location service to read & write
+     * @param sceneService the glyphservice to write
+     */
+    void setServices (SelectionService locationService,
+                      SelectionService sceneService);
+
+    /**
+     * Include the content of runs table to the lag.
+     * @param runsTable the populated runs
+     */
+    void addRuns (RunsTable runsTable);
+
+    /**
+     * Create a section in the lag (using the defined vertexClass)
+     *
+     * @param firstPos the starting position of the section
+     * @param firstRun the very first run of the section
+     *
+     * @return the created section
+     */
+    Section createSection (int firstPos,
+                           Run firstRun);
+
+    /**
+     * Lookup for sections that are contained in the provided
+     * rectangle. Specific sections are not considered.
+     *
+     * @param rect the given rectangle
+     *
+     * @return the set of sections found, which may be empty
+     */
+    Set<Section> lookupSections (PixelRectangle rect);
+
+    /**
+     * Purge the lag of all sections for which provided predicate applies
+     *
+     * @param predicate means to specify whether a section applies for purge
+     *
+     * @return the list of sections purged in this call
+     */
+    List<Section> purgeSections (Predicate<Section> predicate);
+
+    /**
+     * Purge the lag from section with a too small foreground weight, provided
+     * they do not cut larger glyphs
+     *
+     * @param minForeWeight
+     * @return the purged sections
+     */
+    List<Section> purgeTinySections (final int minForeWeight);
+}
