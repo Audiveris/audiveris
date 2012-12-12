af022f18bd9c181ff8a384d31c5696e48e96af17
maximumspatium <maximumspatium@googlemail.com>
2012-12-12 02:39:36 +0100

    Check the availability of Java 7 in the Unix wrapper script. Display a user-friendly error message if Java 7 is missing.

--- dev/unix/audiveris.sh
+++ dev/unix/audiveris.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# check for Java 7 and bail out if not available
+JAVA_VERSION=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+if [ "$JAVA_VERSION" -lt 17 ]; then
+    echo "This version of Audiveris requires Java 7. Please install a Java 7 JRE and try again."
+    exit 1
+fi
+
 PROG_DIR=$(dirname $0)/AudiverisApp/
 EXEC=$PROG_DIR/dist/audiveris.jar
 
