3c29601c695eed38d427a5c2ca9f086c7b23d994
maximumspatium <maximumspatium@googlemail.com>
2013-01-29 02:50:29 +0100

    Basic system for building Debian packages using jdeb task.

--- dev/unix/audiveris.sh
+++ dev/unix/audiveris.sh
@@ -7,19 +7,9 @@ if [ "$JAVA_VERSION" -lt 17 ]; then
     exit 1
 fi
 
-PROG_DIR=$(dirname $0)/AudiverisApp/
+PROG_DIR=/usr/share/audiveris
 EXEC=$PROG_DIR/dist/audiveris.jar
 
-# Select JNI library according with the host hardware architecture
-if [ `getconf LONG_BIT` = "64" ]
-then
-    echo "64bit TessBridge JNI has been activated"
-    JNI_DIR=$PROG_DIR/TessBridgeJNI/64bit/
-else
-    echo "32bit TessBridge JNI has been activated"
-    JNI_DIR=$PROG_DIR/TessBridgeJNI/32bit/
-fi
-
 # Run Audiveris
-LC_ALL=C java -Djava.library.path=$JNI_DIR -jar $EXEC "$@"
+LC_ALL=C java -jar $EXEC "$@"
 
