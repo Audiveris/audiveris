19e904b10455863f06e00a88e0c96fd736259e96
hbitteur <herve.bitteur@audiveris.org>
2013-09-06 21:46:00 +0200

    First features of Audiveris 5 (continued)

---
+++ src/main/omr/ui/util/ItemRenderer.java
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                            I t e m R e n d e r e r                         //
+//                                                                            //
+//----------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">                          //
+//  Copyright © Herve Bitteur and others 2000-2013. All rights reserved.      //
+//  This software is released under the GNU General Public License.           //
+//  Goto http://kenai.com/projects/audiveris to report bugs or suggestions.   //
+//----------------------------------------------------------------------------//
+// </editor-fold>
+package omr.ui.util;
+
+import java.awt.Graphics2D;
+
+/**
+ * Describes a class as able to render specific items of its own
+ * upon a provided graphics environment.
+ *
+ * @author herve
+ */
+public interface ItemRenderer
+{
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * Render items on the provided graphics
+     *
+     * @param graphics the graphics environment
+     */
+    void renderItems (Graphics2D graphics);
+}
---
+++ src/main/omr/ui/util/WeakItemRenderer.java
@@ -0,0 +1,56 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+//                        W e a k I t e m R e n d e r e r                     //
+//                                                                            //
+//----------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">                          //
+//  Copyright © Herve Bitteur and others 2000-2013. All rights reserved.      //
+//  This software is released under the GNU General Public License.           //
+//  Goto http://kenai.com/projects/audiveris to report bugs or suggestions.   //
+//----------------------------------------------------------------------------//
+// </editor-fold>
+package omr.ui.util;
+
+import java.awt.Graphics2D;
+import java.lang.ref.WeakReference;
+
+/**
+ * Provides a way to weakly register a renderer.
+ *
+ * @author Hervé Bitteur
+ */
+public class WeakItemRenderer
+        implements ItemRenderer
+{
+    //~ Instance fields --------------------------------------------------------
+
+    protected final WeakReference<ItemRenderer> weakRenderer;
+
+    //~ Constructors -----------------------------------------------------------
+    //------------------//
+    // WeakItemRenderer //
+    //------------------//
+    /**
+     * Creates a new WeakItemRenderer object.
+     *
+     * @param renderer the actual renderer
+     */
+    public WeakItemRenderer (ItemRenderer renderer)
+    {
+        weakRenderer = new WeakReference<ItemRenderer>(renderer);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+    //-------------//
+    // renderItems //
+    //-------------//
+    @Override
+    public void renderItems (Graphics2D g)
+    {
+        ItemRenderer renderer = weakRenderer.get();
+
+        if (renderer != null) {
+            renderer.renderItems(g);
+        }
+    }
+}
