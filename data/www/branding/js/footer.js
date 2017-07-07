50a4d8d0a7c8d0b6a3fed2826545ab330c798117
Hervé Bitteur <herve.bitteur@laposte.net>
2017-07-07 19:43:13 +0200

    removed old material & doc

--- data/www/branding/js/footer.js
+++
@@ -1,9 +0,0 @@
-/* -------------------------------------------------------------------------- */
-/*                              f o o t e r . j s                             */
-/* -------------------------------------------------------------------------- */
-document.write("<footer>");
-document.write("Audiveris logo by <a href='mailto:katka4@free.fr'>Catherine Bitteur</a>");
-document.write("<br/>");
-document.write("Licensed under the <a href='http://www.gnu.org/licenses/gpl-2.0.html#SEC1'>GNU GPL V2</a> license,");
-document.write(" &copy; 2000-2013 <a href='mailto:herve.bitteur@audiveris.org'>Hervé Bitteur</a> and others");
-document.write("</footer>");
