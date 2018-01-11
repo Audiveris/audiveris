f96ae5baacd8e843dfe9241cb3b89a12bee45890
Hervé Bitteur <herve.bitteur@laposte.net>
2018-01-11 18:41:40 +0100

    Undo/redo for word/sentence modifications (added)

---
+++ src/main/org/audiveris/omr/sig/ui/SentenceTask.java
@@ -0,0 +1,114 @@
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                     S e n t e n c e T a s k                                    //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">
+//
+//  Copyright ©  Audiveris 2017. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the
+//  GNU Affero General Public License as published by the Free Software Foundation, either version
+//  3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this
+//  program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------------------------//
+// </editor-fold>
+package org.audiveris.omr.sig.ui;
+
+import org.audiveris.omr.sig.inter.SentenceInter;
+import org.audiveris.omr.text.TextRole;
+
+/**
+ * Class {@code SentenceTask}
+ *
+ * @author Hervé Bitteur
+ */
+public class SentenceTask
+        extends InterTask
+{
+    //~ Instance fields ----------------------------------------------------------------------------
+
+    /** Old sentence role. */
+    final TextRole oldRole;
+
+    /** New sentence role. */
+    final TextRole newRole;
+
+    //~ Constructors -------------------------------------------------------------------------------
+    /**
+     * Creates a new {@code SentenceTask} object.
+     *
+     * @param sentence the sentence to modify
+     * @param newRole  the new role for the sentence
+     */
+    public SentenceTask (SentenceInter sentence,
+                         TextRole newRole)
+    {
+        super(sentence.getSig(), sentence, sentence.getBounds(), null);
+        this.newRole = newRole;
+
+        oldRole = sentence.getRole();
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+    @Override
+    public SentenceInter getInter ()
+    {
+        return (SentenceInter) inter;
+    }
+
+    /**
+     * @return the newRole
+     */
+    public TextRole getNewRole ()
+    {
+        return newRole;
+    }
+
+    /**
+     * @return the oldRole
+     */
+    public TextRole getOldRole ()
+    {
+        return oldRole;
+    }
+
+    @Override
+    public void performDo ()
+    {
+        getInter().setRole(newRole);
+
+        sheet.getInterIndex().publish(getInter());
+    }
+
+    @Override
+    public void performUndo ()
+    {
+        getInter().setRole(oldRole);
+
+        sheet.getInterIndex().publish(getInter());
+    }
+
+    @Override
+    public String toString ()
+    {
+        StringBuilder sb = new StringBuilder(actionName());
+        sb.append(" ").append(inter);
+        sb.append(" from ").append(oldRole);
+        sb.append(" to ").append(newRole);
+
+        return sb.toString();
+    }
+
+    @Override
+    protected String actionName ()
+    {
+        return "role";
+    }
+}
---
+++ src/main/org/audiveris/omr/sig/ui/WordTask.java
@@ -0,0 +1,97 @@
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                         W o r d T a s k                                        //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">
+//
+//  Copyright ©  Audiveris 2017. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the
+//  GNU Affero General Public License as published by the Free Software Foundation, either version
+//  3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this
+//  program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------------------------//
+// </editor-fold>
+package org.audiveris.omr.sig.ui;
+
+import org.audiveris.omr.sig.inter.WordInter;
+
+/**
+ * Class {@code WordTask} handle the text update of a word.
+ *
+ * @author Hervé Bitteur
+ */
+public class WordTask
+        extends InterTask
+{
+    //~ Instance fields ----------------------------------------------------------------------------
+
+    /** Old word content. */
+    private final String oldValue;
+
+    /** New word content. */
+    private final String newValue;
+
+    //~ Constructors -------------------------------------------------------------------------------
+    /**
+     * Creates a new {@code WordTask} object.
+     *
+     * @param word     the word to modify
+     * @param newValue new word value
+     */
+    public WordTask (WordInter word,
+                     String newValue)
+    {
+        super(word.getSig(), word, word.getBounds(), null);
+        this.newValue = newValue;
+
+        oldValue = word.getValue();
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+    @Override
+    public WordInter getInter ()
+    {
+        return (WordInter) inter;
+    }
+
+    @Override
+    public void performDo ()
+    {
+        getInter().setValue(newValue);
+
+        sheet.getInterIndex().publish(getInter());
+    }
+
+    @Override
+    public void performUndo ()
+    {
+        getInter().setValue(oldValue);
+
+        sheet.getInterIndex().publish(getInter());
+    }
+
+    @Override
+    public String toString ()
+    {
+        StringBuilder sb = new StringBuilder(actionName());
+        sb.append(" ").append(inter);
+        sb.append(" from \"").append(oldValue).append("\"");
+        sb.append(" to \"").append(newValue).append("\"");
+
+        return sb.toString();
+    }
+
+    @Override
+    protected String actionName ()
+    {
+        return "word";
+    }
+}
