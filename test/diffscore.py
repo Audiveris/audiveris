7bb218ab082f12d06dd00115bcb0ec51f060fb83
Alexander Myltsev <a.myltsev@gmail.com>
2018-03-12 16:37:40 +0300

    diffscore.py: support processing the cases/ directory.

--- test/diffscore.py
+++ test/diffscore.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+import os
 import sys
-import music21
+import tempfile
 import logging
+import subprocess
+import argparse
 from collections import defaultdict, namedtuple
+import music21
 
 
 Note = namedtuple('Note', 'pitch offset duration')
@@ -28,7 +32,7 @@ def diffMeasures(measure1, measure2):
     right = measure2 - measure1
     logging.debug('Common: {}, Left: {}, Right: {}'.format(common, left, right))
     score = len(common) - len(left) - len(right)
-    logging.debug('Score:', score)
+    logging.debug('Score: %s', score)
     return score
 
 
@@ -36,15 +40,57 @@ def diffRolls(roll1, roll2):
     score = 0
     barNumbers = sorted(set(roll1.keys()) | set(roll2.keys()))
     for n in barNumbers:
-        logging.debug('Measure', n)
+        logging.debug('Measure %d', n)
         score += diffMeasures(roll1.get(n, set()), roll2.get(n, set()))
     return score
 
 
-def main():
-    files = sys.argv[1:3]
+def diffFiles(files):
     rolls = [pianoroll(music21.converter.parseFile(f)) for f in files]
-    logging.info('Total score:', diffRolls(rolls[0], rolls[1]))
+    score = diffRolls(rolls[0], rolls[1])
+    logging.info('Score for {}: {}'.format(files, score))
+    return score
+
+
+def find_file(dir, pattern):
+    head, _, tail = pattern.partition('*')
+    for d, _, files in os.walk(dir):
+        for f in files:
+            if f.startswith(head) and f.endswith(tail):
+                return os.path.abspath(os.path.join(d, f))
+    raise Exception('file matching "{}*" not found in {}'.format(pattern, dir))
+
+
+def processCases(args):
+    score = 0
+    for case in os.listdir(args.cases):
+        logging.info('Processing case ' + case)
+        casedir = os.path.join(args.cases, case)
+        casefiles = os.listdir(casedir)
+        source = find_file(casedir, 'source*')
+        target = find_file(casedir, 'target*')
+        with tempfile.TemporaryDirectory() as tmpdir:
+            script = args.script.replace('$@', tmpdir).replace('$<', source)
+            logging.debug('Calling script: ' + script)
+            subprocess.check_call(script.split(), cwd='..')
+            result = find_file(tmpdir, '*.mxl')
+            score += diffFiles((result, target))
+    return score
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--cases', help='directory with test cases')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('--script', help='command to recognize a given score',
+                        default='gradle run -PcmdLineArgs=-batch,-export,-output,$@,--,$<')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    score = processCases(args)
+    print('Total score:', score)
 
 
 if __name__ == '__main__':
