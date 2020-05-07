05f77ef2fc2cc077ce74be7c265918faf438d447
Hervé Bitteur <herve.bitteur@audiveris.org>
2020-05-07 13:26:59 +0200

    Support for KEY_CANCEL type of key signature (complement)

---
+++ src/main/org/audiveris/omr/sig/ui/HorizontalEditor.java
@@ -0,0 +1,99 @@
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                 H o r i z o n t a l E d i t o r                                //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">
+//
+//  Copyright © Audiveris 2019. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the
+//  GNU Affero General Public License as published by the Free Software Foundation, either version
+//  3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this
+//  program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------------------------//
+// </editor-fold>
+package org.audiveris.omr.sig.ui;
+
+import org.audiveris.omr.math.PointUtil;
+import org.audiveris.omr.sig.inter.Inter;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ * Class {@code HorizontalEditor} provides just one handle to globally move the inter
+ * horizontally only.
+ *
+ * @author Hervé Bitteur
+ */
+public class HorizontalEditor
+        extends InterEditor
+{
+
+    //~ Static fields/initializers -----------------------------------------------------------------
+    //~ Instance fields ----------------------------------------------------------------------------
+    // Original data
+    private final Rectangle originalBounds;
+
+    // Latest data
+    private final Rectangle latestBounds;
+
+    //~ Constructors -------------------------------------------------------------------------------
+    public HorizontalEditor (final Inter inter)
+    {
+        super(inter);
+
+        originalBounds = inter.getBounds();
+        latestBounds = inter.getBounds();
+
+        // Middle handle: move horizontally only
+        handles.add(selectedHandle = new Handle(inter.getCenter())
+        {
+            @Override
+            public boolean move (Point vector)
+            {
+                final double dx = vector.getX();
+
+                if (dx == 0) {
+                    return false;
+                }
+
+                // Data
+                latestBounds.x += dx;
+
+                // Handle
+                for (Handle handle : handles) {
+                    PointUtil.add(handle.getHandleCenter(), dx, 0);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+    @Override
+    protected void doit ()
+    {
+        inter.setBounds(latestBounds);
+
+        super.doit(); // No more glyph
+    }
+
+    @Override
+    public void undo ()
+    {
+        inter.setBounds(originalBounds);
+
+        super.undo();
+    }
+
+    //~ Inner Classes ------------------------------------------------------------------------------
+}
---
+++ src/main/org/audiveris/omr/ui/symbol/KeyCancelSymbol.java
@@ -0,0 +1,76 @@
+//------------------------------------------------------------------------------------------------//
+//                                                                                                //
+//                                  K e y C a n c e l S y m b o l                                 //
+//                                                                                                //
+//------------------------------------------------------------------------------------------------//
+// <editor-fold defaultstate="collapsed" desc="hdr">
+//
+//  Copyright © Audiveris 2019. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify it under the terms of the
+//  GNU Affero General Public License as published by the Free Software Foundation, either version
+//  3 of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License along with this
+//  program.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------------------------//
+// </editor-fold>
+package org.audiveris.omr.ui.symbol;
+
+import org.audiveris.omr.glyph.Shape;
+import org.audiveris.omr.sig.inter.ClefInter.ClefKind;
+
+/**
+ * Class {@code KeyCancelSymbol} displays a Key Signature cancel symbol.
+ *
+ * @author Hervé Bitteur
+ */
+public class KeyCancelSymbol
+        extends KeySymbol
+{
+
+    //~ Static fields/initializers -----------------------------------------------------------------
+    //~ Instance fields ----------------------------------------------------------------------------
+    public ClefKind clefKind;
+
+    //~ Constructors -------------------------------------------------------------------------------
+    /**
+     * Creates a new {@code KeyCancelSymbol} object
+     *
+     * @param isIcon true for an icon
+     */
+    public KeyCancelSymbol (boolean isIcon)
+    {
+        super(0, isIcon, Shape.KEY_CANCEL, 110);
+    }
+
+    /**
+     * Creates a KeyCancelSymbol with count and location of naturals based on the
+     * provided {@link ClefKind} and precise key fifths to cancel.
+     *
+     * @param clefKind the active clef kind
+     * @param fifths   the canceled key
+     */
+    public KeyCancelSymbol (ClefKind clefKind,
+                            int fifths)
+    {
+        super(fifths, false, Shape.KEY_CANCEL, 110);
+        this.clefKind = clefKind;
+    }
+
+    //~ Methods ------------------------------------------------------------------------------------
+    //------------//
+    // createIcon //
+    //------------//
+    @Override
+    protected ShapeSymbol createIcon ()
+    {
+        return new KeyCancelSymbol(true);
+    }
+
+    //~ Inner Classes ------------------------------------------------------------------------------
+}
