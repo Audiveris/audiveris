fc7f29b3e5deff547f2c05de634006ed9b80bc3c
Hervé Bitteur <herve.bitteur@audiveris.org>
2021-03-16 17:43:02 +0100

    Bumped copyright to 2021

--- src/main/org/audiveris/omr/sheet/stem/StemLinker.java
+++ src/main/org/audiveris/omr/sheet/stem/StemLinker.java
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------------------------//
 // <editor-fold defaultstate="collapsed" desc="hdr">
 //
-//  Copyright © Audiveris 2019. All rights reserved.
+//  Copyright © Audiveris 2021. All rights reserved.
 //
 //  This program is free software: you can redistribute it and/or modify it under the terms of the
 //  GNU Affero General Public License as published by the Free Software Foundation, either version
